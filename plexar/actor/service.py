36efc13f7599d9a615f3aba3330c238f3a3f046a
Jiayi <nijiayi1119@outlook.com>
2023-06-28 11:28:51 +0000

    fix restapi

--- plexar/actor/service.py
+++ plexar/actor/service.py
@@ -20,10 +20,11 @@ import xoscar as xo
 from plexar.actor import ModelActor
 from plexar.model import ModelSpec
 
-from fastapi import FastAPI, APIRouter
+from fastapi import FastAPI, APIRouter, Request
 from fastapi import HTTPException
 import asyncio
 from uvicorn import Config, Server
+import uuid
 
 logger = getLogger(__name__)
 
@@ -129,60 +130,57 @@ class ControllerActor(xo.Actor):
 
 
 class RESTAPIActor(xo.Actor):
-    def __init__(self, addr: str):
+    def __init__(self, host:str, port:int):
         super().__init__()
-        self._address = addr
         self._controller_ref = None
         app = FastAPI()
         self.router = APIRouter()
         self.router.add_api_route("/models", self.list_models, methods=["GET"])
-        self.router.add_api_route("/models/{model_uid}", self.get_model, methods=["GET"])
         self.router.add_api_route("/models", self.launch_model, methods=["POST"])
         self.router.add_api_route("/models/{model_uid}", self.terminate_model, methods=["DELETE"])
         app.include_router(self.router)
 
         # uvicorn
         loop = asyncio.get_event_loop()
-        config = Config(app=app, loop=loop, host="0.0.0.0", port=8000)
+        config = Config(app=app, loop=loop, host=host, port=port)
         server = Server(config)
         loop.create_task(server.serve())
-
-    async def _start_controller(self):
+    
+    async def __post_create__(self):
         self._controller_ref = await xo.actor_ref(
-            address=self._address, uid=ControllerActor.uid()
+            address=self.address, uid=ControllerActor.uid()
         )
+    
+    def gen_model_uid(self) -> str:
+        # generate a time-based uuid.
+        return str(uuid.uuid1())
 
     async def list_models(self) -> List[str]:
-        await self._start_controller()
-        return await self._controller_ref.list_models()
+        models = await self._controller_ref.list_models()
+        return [model_uid for model_uid, _ in models]
 
-    async def get_model(self, model_uid: str):
-        await self._start_controller()
-        return await self._controller_ref.get_model(model_uid)
 
-    async def launch_model(
-        self,
-        model_uid: str = None,
-        model_name: str = None,
-        n_parameters_in_billions: int = None,
-        fmt: str = None,
-        quantization: str = None
-    ):
-        await self._start_controller()
-        if model_uid is None:
-            model_uid = self._controller_ref.gen_model_uid()
+    async def launch_model(self, request: Request) -> str:
+        payload = await request.json()
+        model_name = payload.get('model_name')
+        n_parameters_in_billions = payload.get('n_parameters_in_billions')
+        fmt = payload.get('fmt')
+        quantization = payload.get('quantization')
+        kwargs = payload.get('kwargs', {}) or {}
+
+        model_uid = self.gen_model_uid()
 
         await self._controller_ref.launch_builtin_model(
             model_uid=model_uid,
             model_name=model_name,
             n_parameters_in_billions=n_parameters_in_billions,
             fmt=fmt,
-            quantization=quantization
+            quantization=quantization,
+            **kwargs
         )
-        return {"model_uid": model_uid}
+        return model_uid
 
     async def terminate_model(self, model_uid: str):
-        await self._start_controller()
         await self._controller_ref.terminate_model(model_uid)
         return {"message": "Model terminated successfully."}
 
@@ -267,3 +265,305 @@ class WorkerActor(xo.Actor):
         assert model_uid in self._model_uid_to_model
 
         return self._model_uid_to_model[model_uid]
+
+
+
+
+# Copyright 2022-2023 XProbe Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# from logging import getLogger
+# from typing import Callable, Dict, List, Optional
+
+# import xoscar as xo
+
+# from plexar.actor import ModelActor
+# from plexar.model import ModelSpec
+
+# from fastapi import FastAPI, APIRouter, Request
+# from fastapi import HTTPException
+# import asyncio
+# from uvicorn import Config, Server
+# import uuid
+
+# logger = getLogger(__name__)
+
+# class Isolation:
+#     # TODO: better move isolation to xoscar.
+#     def __init__(self, loop: asyncio.AbstractEventLoop, threaded: bool = True):
+#         self._loop = loop
+#         self._threaded = threaded
+
+#         self._stopped = None
+#         self._thread = None
+#         self._thread_ident = None
+
+#     def _run(self):
+#         asyncio.set_event_loop(self._loop)
+#         self._stopped = asyncio.Event()
+#         self._loop.run_until_complete(self._stopped.wait())
+
+#     def start(self):
+#         if self._threaded:
+#             self._thread = thread = threading.Thread(target=self._run)
+#             thread.daemon = True
+#             thread.start()
+#             self._thread_ident = thread.ident
+
+#     def call(self, coro: Coroutine) -> Any:
+#         fut = asyncio.run_coroutine_threadsafe(coro, self._loop)
+#         return fut.result()
+
+#     @property
+#     def thread_ident(self):
+#         return self._thread_ident
+
+#     async def _stop(self):
+#         self._stopped.set()
+
+#     def stop(self):
+#         if self._threaded:
+#             asyncio.run_coroutine_threadsafe(self._stop(), self._loop).result()
+#             self._thread.join()
+
+
+# def log(func: Callable):
+#     import time
+#     from functools import wraps
+
+#     @wraps(func)
+#     def wrapped(*args, **kwargs):
+#         logger.debug(f"Enter {func.__name__}, args: {args}, kwargs: {kwargs}")
+#         start = time.time()
+#         ret = func(*args, **kwargs)
+#         logger.debug(
+#             f"Leave {func.__name__}, elapsed time: {int(time.time() - start)} ms"
+#         )
+#         return ret
+
+#     return wrapped
+
+
+# class ControllerActor(xo.Actor):
+#     def __init__(self):
+#         super().__init__()
+#         self._worker_address_to_worker: Dict[str, xo.ActorRefType[WorkerActor]] = {}
+#         self._model_uid_to_worker: Dict[str, xo.ActorRefType[WorkerActor]] = {}
+
+#     @classmethod
+#     def uid(cls) -> str:
+#         return "plexar_controller"
+
+#     async def _choose_worker(self) -> xo.ActorRefType["WorkerActor"]:
+#         # TODO: better allocation strategy.
+#         min_running_model_count = None
+#         target_worker = None
+#         for worker in self._worker_address_to_worker.values():
+#             running_model_count = await worker.get_model_count()
+#             if (
+#                 min_running_model_count is None
+#                 or running_model_count < min_running_model_count
+#             ):
+#                 min_running_model_count = running_model_count
+#                 target_worker = worker
+
+#             return target_worker
+
+#         raise RuntimeError("TODO")
+
+#     @log
+#     async def launch_builtin_model(
+#         self,
+#         model_uid: str,
+#         model_name: str,
+#         n_parameters_in_billions: Optional[int],
+#         fmt: Optional[str],
+#         quantization: Optional[str],
+#         **kwargs,
+#     ) -> xo.ActorRefType["ModelActor"]:
+#         assert model_uid not in self._model_uid_to_worker
+
+#         worker_ref = await self._choose_worker()
+#         model_ref = await worker_ref.launch_builtin_model(
+#             model_uid=model_uid,
+#             model_name=model_name,
+#             n_parameters_in_billions=n_parameters_in_billions,
+#             fmt=fmt,
+#             quantization=quantization,
+#             **kwargs,
+#         )
+#         self._model_uid_to_worker[model_uid] = worker_ref
+
+#         return model_ref
+
+#     @log
+#     async def terminate_model(self, model_uid: str):
+#         assert model_uid in self._model_uid_to_worker
+
+#         worker_ref = self._model_uid_to_worker[model_uid]
+#         await worker_ref.terminate_model(model_uid=model_uid)
+#         del self._model_uid_to_worker[model_uid]
+
+#     @log
+#     async def get_model(self, model_uid: str):
+#         assert model_uid in self._model_uid_to_worker
+
+#         worker_ref = self._model_uid_to_worker[model_uid]
+#         return await worker_ref.get_model(model_uid=model_uid)
+
+#     @log
+#     async def list_models(self) -> List[tuple[str, ModelSpec]]:
+#         ret = []
+#         for worker in self._worker_address_to_worker.values():
+#             ret.extend(await worker.list_models())
+#         return ret
+
+#     @log
+#     async def add_worker(self, worker_address: str):
+#         assert worker_address not in self._worker_address_to_worker
+
+#         worker_ref = await xo.actor_ref(address=worker_address, uid=WorkerActor.uid())
+#         self._worker_address_to_worker[worker_address] = worker_ref
+
+
+# class RESTAPIActor(xo.Actor):
+#     def __init__(self, host:str, port:int):
+#         super().__init__()
+#         self._controller_ref = None
+#         app = FastAPI()
+#         self.router = APIRouter()
+#         self.router.add_api_route("/models", self.list_models, methods=["GET"])
+#         self.router.add_api_route("/models/{model_uid}", self.get_model, methods=["GET"])
+#         self.router.add_api_route("/models", self.launch_model, methods=["POST"])
+#         self.router.add_api_route("/models/{model_uid}", self.terminate_model, methods=["DELETE"])
+#         app.include_router(self.router)
+
+#         # uvicorn
+#         loop = asyncio.get_event_loop()
+#         config = Config(app=app, loop=loop, host=host, port=port)
+#         server = Server(config)
+#         loop.create_task(server.serve())
+    
+#     async def __post_create__(self):
+#         self._controller_ref = await xo.actor_ref(
+#             address=self.address, uid=ControllerActor.uid()
+#         )
+    
+#     def gen_model_uid(self) -> str:
+#         # generate a time-based uuid.
+#         return str(uuid.uuid1())
+
+#     async def list_models(self) -> List[str]:
+#         return await self._controller_ref.list_models()
+
+#     async def get_model(self, model_uid: str):
+#         return await self._controller_ref.get_model(model_uid)
+
+#     async def launch_model(self, request: Request) -> str:
+#         payload = await request.json()
+#         model_name = payload.get('model_name')
+#         n_parameters_in_billions = payload.get('n_parameters_in_billions')
+#         fmt = payload.get('fmt')
+#         quantization = payload.get('quantization')
+#         kwargs = payload.get('kwargs', {}) or {}
+
+#         model_uid = self.gen_model_uid()
+
+#         await self._controller_ref.launch_builtin_model(
+#             model_uid=model_uid,
+#             model_name=model_name,
+#             n_parameters_in_billions=n_parameters_in_billions,
+#             fmt=fmt,
+#             quantization=quantization,
+#             **kwargs
+#         )
+#         return model_uid
+
+#     async def terminate_model(self, model_uid: str):
+#         await self._controller_ref.terminate_model(model_uid)
+#         return {"message": "Model terminated successfully."}
+
+
+# class WorkerActor(xo.Actor):
+#     def __init__(self, controller_address: str):
+#         super().__init__()
+#         self._controller_address = controller_address
+#         self._model_uid_to_model: Dict[str, xo.ActorRefType["ModelActor"]] = {}
+#         self._model_uid_to_model_spec: Dict[str, ModelSpec] = {}
+
+#     @classmethod
+#     def uid(cls) -> str:
+#         return "plexar_worker"
+
+#     async def __post_create__(self):
+#         controller_ref: xo.ActorRefType["ControllerActor"] = await xo.actor_ref(
+#             address=self._controller_address, uid=ControllerActor.uid()
+#         )
+#         await controller_ref.add_worker(self.address)
+
+#     async def get_model_count(self) -> int:
+#         return len(self._model_uid_to_model)
+
+#     @log
+#     async def launch_builtin_model(
+#         self,
+#         model_uid: str,
+#         model_name: str,
+#         n_parameters_in_billions: Optional[int],
+#         fmt: Optional[str],
+#         quantization: Optional[str],
+#         **kwargs,
+#     ) -> xo.ActorRefType["ModelActor"]:
+#         assert model_uid not in self._model_uid_to_model
+
+#         from plexar.model import MODEL_SPECS
+
+#         for model_spec in MODEL_SPECS:
+#             if model_spec.match(
+#                 model_name, n_parameters_in_billions, fmt, quantization
+#             ):
+#                 logger.debug(f"Matched model sepc for %s: %s", model_uid, model_spec)
+
+#                 model_cls = model_spec.cls
+#                 assert model_cls is not None
+
+#                 save_path = model_spec.cache()
+#                 model = model_cls(save_path, kwargs)
+#                 model_ref = await xo.create_actor(
+#                     ModelActor, address=self.address, uid=model_uid, model=model
+#                 )
+#                 self._model_uid_to_model[model_uid] = model_ref
+#                 self._model_uid_to_model_spec[model_uid] = model_spec
+#                 return model_ref
+
+#         raise ValueError("TODO")
+
+#     @log
+#     async def terminate_model(self, model_uid: str):
+#         assert model_uid in self._model_uid_to_model
+
+#         model_ref = self._model_uid_to_model[model_uid]
+#         await xo.destroy_actor(model_ref)
+#         del self._model_uid_to_model[model_uid]
+
+#     @log
+#     async def list_models(self) -> List[tuple[str, ModelSpec]]:
+#         return list(self._model_uid_to_model_spec.items())
+
+#     @log
+#     async def get_model(self, model_uid: str) -> xo.ActorRefType["ModelActor"]:
+#         assert model_uid in self._model_uid_to_model
+
+#         return self._model_uid_to_model[model_uid]
+
--- plexar/deploy/controller.py
+++ plexar/deploy/controller.py
@@ -22,7 +22,7 @@ from ..actor.service import ControllerActor, RESTAPIActor
 
 async def start_controller_components(address: str, share: bool, host: str, port: int):
     await xo.create_actor(ControllerActor, address=address, uid=ControllerActor.uid())
-    await xo.create_actor(RESTAPIActor, address=address, uid="restful", addr = "0.0.0.0:8000")
+    await xo.create_actor(RESTAPIActor, address=address, uid="restful", host="0.0.0.0", port=8000)
     gradio = await xo.create_actor(
         GradioActor,
         xoscar_endpoint=address,
