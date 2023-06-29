a22f9e7048b0a53663da1eab4099fa860f1527e7
Jiayi <nijiayi1119@outlook.com>
2023-06-29 05:58:47 +0000

    merge

--- plexar/deploy/controller.py
+++ plexar/deploy/controller.py
@@ -1,6 +1,27 @@
+# Copyright 2022-2023 XProbe Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import asyncio
+
+import xoscar as xo
+
+from ..actor.gradio import GradioActor
+from ..actor.service import ControllerActor, RESTAPIActor
+
 async def start_controller_components(address: str, share: bool, host: str, port: int):
     await xo.create_actor(ControllerActor, address=address, uid=ControllerActor.uid())
-    rest_ref = await xo.create_actor(RESTAPIActor, address=address, uid="restful", host="0.0.0.0", port=8000)
+    await xo.create_actor(RESTAPIActor, address=address, uid="restful", host=host, port=port)
     gradio = await xo.create_actor(
         GradioActor,
         xoscar_endpoint=address,
