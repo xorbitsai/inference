9a9f4eea837730125d85e882febb51e1ed3ec74c
Rui Ji <ruiji@usc.edu>
2023-07-05 16:19:51 +0800

    Test Uploaded for chatting.

--- plexar/deploy/test/test_cmdline.py
+++ plexar/deploy/test/test_cmdline.py
@@ -17,7 +17,7 @@ import pytest
 from click.testing import CliRunner
 
 from plexar.client import Client
-from plexar.deploy.cmdline import model_generate
+from plexar.deploy.cmdline import model_chat, model_generate
 
 
 @pytest.mark.asyncio
@@ -41,3 +41,24 @@ async def test_generate(setup):
 
     assert result.exit_code == 0
     assert len(result.stdout) != 0
+
+
+async def test_chat(setup):
+    pool = setup
+    address = pool.external_address
+    client = Client(address)
+    model_uid = client.launch_model("wizardlm-v1.0", quantization="q4_0")
+    assert model_uid is not None
+
+    runner = CliRunner()
+    result = runner.invoke(
+        model_chat,
+        [
+            "--model-uid",
+            model_uid,
+        ],
+        input="Write a poem.\nexit\n",
+    )
+
+    assert result.exit_code == 0
+    print(result)
