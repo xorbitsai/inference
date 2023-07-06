fcc1c08fdb79e06685f8b159203adda6d856d5a1
aresnow <aresnow1@gmail.com>
2023-07-06 20:32:25 +0800

    revert

--- xinference/conftest.py
+++ xinference/conftest.py
@@ -21,16 +21,16 @@ from .core.service import SupervisorActor, WorkerActor
 
 @pytest_asyncio.fixture
 async def setup():
-    address = "127.0.0.1"
+    address = "127.0.0.1:9998"
     pool = await xo.create_actor_pool(address, n_process=0)
     await xo.create_actor(
         SupervisorActor, address=pool.external_address, uid=SupervisorActor.uid()
     )
     await xo.create_actor(
         WorkerActor,
-        address=pool.external_address,
+        address=address,
         uid=WorkerActor.uid(),
-        supervisor_address=pool.external_address,
+        supervisor_address=address,
     )
 
     async with pool:
