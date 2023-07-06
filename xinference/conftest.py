79181dc7ca020a875060c9f47426c0a4f1ee26ec
aresnow <aresnow1@gmail.com>
2023-07-06 20:02:26 +0800

    Add workflow for uploading to pypi

--- xinference/conftest.py
+++ xinference/conftest.py
@@ -21,7 +21,7 @@ from .core.service import SupervisorActor, WorkerActor
 
 @pytest_asyncio.fixture
 async def setup():
-    address = "127.0.0.1:9998"
+    address = "127.0.0.1"
     pool = await xo.create_actor_pool(address, n_process=0)
     await xo.create_actor(
         SupervisorActor, address=pool.external_address, uid=SupervisorActor.uid()
