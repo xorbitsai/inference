ac66e9bd37978cd141c94b3eb1214c45e7901b9e
Jiayi Ni <nijiayi1119@outlook.com>
2023-07-10 03:04:47 -0700

    add tests

--- xinference/core/tests/test_restful_api.py
+++ xinference/core/tests/test_restful_api.py
@@ -22,6 +22,59 @@ async def test_restful_api(setup):
     print(endpoint)
     url = f"{endpoint}/v1/models"
 
+    # list
     response = requests.get(url)
     response_data = response.json()
     assert len(response_data) == 0
+
+    # launch
+    payload = {"model_uid": "test", "model_name": "orca", "quantization": "q4_0"}
+
+    response = requests.post(url, json=payload)
+    response_data = response.json()
+    model_uid_res = response_data["model_uid"]
+    assert model_uid_res == "test"
+
+    # list
+    response = requests.get(url)
+    response_data = response.json()
+    assert len(response_data) == 1
+
+    # generate
+    url = f"{endpoint}/v1/completions"
+    payload = {
+        "model": model_uid_res,
+        "prompt": "Once upon a time, there was a very old computer.",
+    }
+    response = requests.post(url, json=payload)
+    completion = response.json()
+    assert "text" in completion["choices"][0]
+
+    # chat
+    url = f"{endpoint}/v1/chat/completions"
+    payload = {
+        "model": model_uid_res,
+        "messages": [
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": "Hello!"},
+            {"role": "assistant", "content": "Hi what can I help you?"},
+            {"role": "user", "content": "What is the capital of France?"},
+        ],
+    }
+    response = requests.post(url, json=payload)
+    completion = response.json()
+    assert "content" in completion["choices"][0]["message"]
+
+    # delete
+    url = f"{endpoint}/v1/models/test"
+    response = requests.delete(url)
+
+    # list
+    response = requests.get(f"{endpoint}/v1/models")
+    response_data = response.json()
+    assert len(response_data) == 0
+
+    # delete again
+    url = f"{endpoint}/v1/models/test"
+    response = requests.delete(url)
+    assert response.status_code != 200
