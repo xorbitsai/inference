77e81c637f777aadea85d0a3f50e656dafd65a3e
Rui Ji <ruiji@usc.edu>
2023-07-14 17:27:43 +0800

    fix test issue

--- xinference/model/tests/test_model_availablity.py
+++ xinference/model/tests/test_model_availablity.py
@@ -120,10 +120,12 @@ def test_model_integrity():
 
     with pytest.warns(Warning) as w:
         test_model.cache()
-        assert len(w) == 1
+        assert len(w) == 2
 
+        warning2 = w[1]
         warning = w[0]
         assert str(warning.message) == "Model size doesn't match, try to update it..."
+        assert str(warning2.message) == "Model size doesn't match, try to update it..."
 
 
 @pytest.mark.skipif(
