b5e92d4ceaf7bebe5548a7549fd9b6791b508396
Bojun-Feng <davidfeng@uchicago.edu>
2023-07-17 10:45:20 +0800

    basic file structure

--- xinference/web/ui/src/App.js
+++ xinference/web/ui/src/App.js
@@ -1,8 +1,17 @@
-
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 function App() {
+  const [theme, colorMode] = useMode();
   return (
-    <div className="App">
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="App">
+          <main className="content">
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
