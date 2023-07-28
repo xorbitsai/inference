6e0a8aae8b4ebdfaec90e319873f6b4734c3ce10
Bojun-Feng <102875484+Bojun-Feng@users.noreply.github.com>
2023-07-28 16:54:32 +0800

    TST: Update ChatGLM Tests (#259)

--- xinference/model/llm/ggml/tests/test_chatglm.py
+++ xinference/model/llm/ggml/tests/test_chatglm.py
@@ -12,109 +12,178 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# import random
-# import string
-# from typing import Iterator, Optional
-#
-# import pytest
-#
-# from xinference.model import ModelFamily, ModelSpec
-# from xinference.model.llm.ggml.chatglm import ChatglmCppChatModel
-#
-#
-# class MockModelFamily(ModelFamily):
-#     def cache(
-#         self,
-#         model_size_in_billions: Optional[int] = None,
-#         quantization: Optional[str] = None,
-#     ):
-#         pass
-#
-#
-# class MockPipeline:
-#     def __init__(self) -> None:
-#         pass
-#
-#     def stream_chat(self, *args, **kwargs) -> Iterator[str]:
-#         res = [f"chatglm_test_stream_{i}" for i in range(100)]
-#         return iter(res)
-#
-#     def chat(self, *args, **kwargs) -> str:
-#         res = "chatglm_test_chat"
-#         return res
-#
-#
-# class MockChatglmCppChatModel(ChatglmCppChatModel):
-#     def load(self):
-#         self._llm = MockPipeline()
-#
-#
-# mock_model_spec1 = ModelSpec(
-#     model_name="chatglm",
-#     model_format="ggmlv3",
-#     model_size_in_billions=6,
-#     quantization="q4_0",
-#     url="http://chatglm_test.url",
-#     rp_url="",
-# )
-#
-# mock_model_spec2 = ModelSpec(
-#     model_name="chatglm2",
-#     model_format="ggmlv3",
-#     model_size_in_billions=6,
-#     quantization="q4_0",
-#     url="http://chatglm2_test.url",
-#     rp_url="",
-# )
-#
-#
-# @pytest.mark.parametrize("model_spec", [mock_model_spec1, mock_model_spec2])
-# def test_model_init(model_spec):
-#     uid = "".join(random.choice(string.digits) for i in range(100))
-#     path = "".join(
-#         random.choice(string.ascii_letters + string.punctuation) for i in range(100)
-#     )
-#     model = MockChatglmCppChatModel(uid, model_spec, path)
-#
-#     assert model.model_uid == uid
-#     assert model._model_path == path
-#     assert model.model_spec == model_spec
-#
-#     assert model._model_config is None
-#     model._model_config = model._sanitize_generate_config(None)
-#     assert model._model_config["stream"] == False
-#
-#
-# @pytest.mark.parametrize("model_spec", [mock_model_spec1, mock_model_spec2])
-# def test_model_pipeline(model_spec):
-#     model = MockChatglmCppChatModel("uid", model_spec, "path")
-#     assert model._llm is None
-#
-#     model.load()
-#     assert isinstance(model._llm, MockPipeline)
-#
-#
-# @pytest.mark.parametrize("model_spec", [mock_model_spec1, mock_model_spec2])
-# def test_chat_stream(model_spec):
-#     model = MockChatglmCppChatModel("uid", model_spec, "path")
-#     model.load()
-#     responses = list(model.chat("Hello", generate_config={"stream": True}))
-#
-#     assert responses[0]["choices"][0]["delta"] == {"role": "assistant"}
-#
-#     for i in range(3):
-#         assert responses[i + 1]["choices"][0]["delta"] == {
-#             "content": f"chatglm_test_stream_{i}"
-#         }
-#
-#
-# @pytest.mark.parametrize("model_spec", [mock_model_spec1, mock_model_spec2])
-# def test_chat_non_stream(model_spec):
-#     model = MockChatglmCppChatModel("uid", model_spec, "path")
-#     model.load()
-#     response = model.chat("Hello")
-#
-#     assert response["choices"][0]["message"] == {
-#         "role": "assistant",
-#         "content": "chatglm_test_chat",
-#     }
+import random
+import string
+from typing import Iterator
+
+import pytest
+
+from xinference.model.llm.ggml.chatglm import ChatglmCppChatModel
+from xinference.model.llm.llm_family import GgmlLLMSpecV1, LLMFamilyV1
+
+
+class MockPipeline:
+    def __init__(self) -> None:
+        pass
+
+    def stream_chat(self, *args, **kwargs) -> Iterator[str]:
+        res = [f"chatglm_test_stream_{i}" for i in range(5)]
+        return iter(res)
+
+    def chat(self, *args, **kwargs) -> str:
+        res = "chatglm_test_chat"
+        return res
+
+
+class MockChatglmCppChatModel(ChatglmCppChatModel):
+    def load(self):
+        self._llm = MockPipeline()
+
+
+mock_model_spec = GgmlLLMSpecV1(
+    model_format="ggmlv3",
+    model_size_in_billions=6,
+    quantizations=["q2_k", "q4_0"],
+    model_id="test_id",
+    model_file_name_template="TestModel.{quantization}.ggmlv3.bin",
+)
+
+serialized = """{
+   "version":1,
+   "model_name":"TestModel",
+   "model_lang":[
+      "en"
+   ],
+   "model_ability":[
+      "embed", "generate"
+   ],
+   "model_specs":[
+      {
+         "model_format":"ggmlv3",
+         "model_size_in_billions":6,
+         "quantizations": ["q2_k", "q4_0"],
+         "model_id":"test_id",
+         "model_file_name_template":"TestModel.{quantization}.ggmlv3.bin"
+      },
+      {
+         "model_format":"pytorch",
+         "model_size_in_billions":3,
+         "quantizations": ["int8", "int4", "none"],
+         "model_id":"example/TestModel"
+      }
+   ],
+   "prompt_style": {
+       "style_name": "ADD_COLON_SINGLE",
+       "system_prompt": "TEST",
+       "roles": ["user", "assistant"],
+       "intra_message_sep": "\\n### ",
+       "inter_message_sep": "\\n### ",
+       "stop": null,
+       "stop_token_ids": null
+   }
+}"""
+
+mock_model_family = LLMFamilyV1.parse_raw(serialized)
+
+
+@pytest.mark.parametrize(
+    "model_spec, model_family", [(mock_model_spec, mock_model_family)]
+)
+def test_model_init(model_spec, model_family):
+    quantization = "q2_k"
+    uid = "".join(random.choice(string.digits) for i in range(100))
+    path = "".join(
+        random.choice(string.ascii_letters + string.punctuation) for i in range(100)
+    )
+    model = MockChatglmCppChatModel(
+        model_uid=uid,
+        model_family=model_family,
+        model_spec=model_spec,
+        quantization=quantization,
+        model_path=path,
+    )
+
+    assert model.model_uid == uid
+    assert model.quantization == quantization
+    assert model.model_path == path
+
+    assert isinstance(model.model_spec, GgmlLLMSpecV1)
+    assert isinstance(model.model_family, LLMFamilyV1)
+    assert isinstance(model.model_family.model_specs[0], GgmlLLMSpecV1)
+
+    assert (
+        model.model_family.model_specs[0].model_format == model.model_spec.model_format
+    )
+    assert model.model_family.model_specs[0].model_format == model_spec.model_format
+    assert (
+        model.model_family.model_specs[0].model_size_in_billions
+        == model.model_spec.model_size_in_billions
+    )
+    assert (
+        model.model_family.model_specs[0].model_size_in_billions
+        == model_spec.model_size_in_billions
+    )
+    assert (
+        model.model_family.model_specs[0].quantizations
+        == model.model_spec.quantizations
+    )
+    assert model.model_family.model_specs[0].quantizations == model_spec.quantizations
+    assert model.model_family.model_specs[0].model_id == model.model_spec.model_id
+    assert model.model_family.model_specs[0].model_id == model_spec.model_id
+    assert (
+        model.model_family.model_specs[0].model_file_name_template
+        == model.model_spec.model_file_name_template
+    )
+    assert (
+        model.model_family.model_specs[0].model_file_name_template
+        == model_spec.model_file_name_template
+    )
+    assert (
+        model.model_family.model_specs[0].model_local_path
+        == model.model_spec.model_local_path
+    )
+    assert (
+        model.model_family.model_specs[0].model_local_path
+        == model_spec.model_local_path
+    )
+
+    assert model._llm is None
+    assert model._model_config is None
+    model._model_config = model._sanitize_generate_config(None)
+    assert not model._model_config["stream"]
+
+
+@pytest.mark.parametrize(
+    "model_spec, model_family", [(mock_model_spec, mock_model_family)]
+)
+def test_model_chat(model_spec, model_family):
+    quantization = "q2_k"
+    uid = "".join(random.choice(string.digits) for i in range(100))
+    path = "".join(
+        random.choice(string.ascii_letters + string.punctuation) for i in range(100)
+    )
+    model = MockChatglmCppChatModel(
+        model_uid=uid,
+        model_family=model_family,
+        model_spec=model_spec,
+        quantization=quantization,
+        model_path=path,
+    )
+
+    assert model._llm is None
+
+    model.load()
+    assert isinstance(model._llm, MockPipeline)
+
+    responses_stream = list(model.chat("Hello", generate_config={"stream": True}))
+    assert responses_stream[0]["choices"][0]["delta"] == {"role": "assistant"}
+    for i in range(3):
+        assert responses_stream[i + 1]["choices"][0]["delta"] == {
+            "content": f"chatglm_test_stream_{i}"
+        }
+
+    responses_non_stream = model.chat("Hello", generate_config={"stream": False})
+    assert responses_non_stream["choices"][0]["message"] == {
+        "role": "assistant",
+        "content": "chatglm_test_chat",
+    }
