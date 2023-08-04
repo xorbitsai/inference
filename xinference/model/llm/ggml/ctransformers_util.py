77d49543d22ab2caa61eeab2d0ec08d8ee05f0b9
Rui Ji <ruiji@usc.edu>
2023-08-04 11:18:11 +0800

    fix rebase

---
+++ xinference/model/llm/ggml/ctransformers_util.py
@@ -0,0 +1,143 @@
+# Copyright 2022-2023 XProbe Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import re
+import time
+import uuid
+from typing import Iterator, Optional, Sequence, Tuple
+
+from ctransformers.utils import utf8_split_incomplete
+
+from xinference.types import CompletionChoice, CompletionChunk, CompletionUsage
+
+
+def _get(*values):
+    for value in values:
+        if value is not None:
+            return value
+
+
+def generate_stream(
+    model,
+    prompt: str,
+    *,
+    max_new_tokens: Optional[int] = None,
+    top_k: Optional[int] = None,
+    top_p: Optional[float] = None,
+    temperature: Optional[float] = None,
+    repetition_penalty: Optional[float] = None,
+    last_n_tokens: Optional[int] = None,
+    seed: Optional[int] = None,
+    batch_size: Optional[int] = None,
+    stream: Optional[bool] = True,
+    threads: Optional[int] = None,
+    stop: Optional[Sequence[str]] = None,
+    reset: Optional[bool] = None,
+) -> Iterator[Tuple[CompletionChunk, CompletionUsage]]:
+    max_new_tokens = _get(max_new_tokens)
+    stop = _get(stop) or []
+    if isinstance(stop, str):
+        stop = [stop]
+
+    tokens = model.tokenize(prompt)
+
+    stop_regex = re.compile("|".join(map(re.escape, stop)))
+    count = 0
+    text = ""
+    incomplete = b""
+
+    # parameters needed for Xinference.
+    finish_reason = None
+
+    for token in model.generate(
+        tokens,
+        top_k=top_k,
+        top_p=top_p,
+        temperature=temperature,
+        repetition_penalty=repetition_penalty,
+        last_n_tokens=last_n_tokens,
+        seed=seed,
+        batch_size=batch_size,
+        threads=threads,
+        reset=reset,
+    ):
+        # Handle incomplete UTF-8 multi-byte characters.
+        incomplete += model.detokenize([token], decode=False)
+        complete, incomplete = utf8_split_incomplete(incomplete)
+        output = complete.decode(errors="ignore")
+        text += output
+
+        # https://github.com/abetlen/llama-cpp-python/blob/1a13d76c487df1c8560132d10bda62d6e2f4fa93/llama_cpp/llama.py#L686-L706
+        # Check if one of the stop sequences is part of the text.
+        # Note that the stop sequence may not always be at the end of text.
+        if stop:
+            match = stop_regex.search(text)
+            if match:
+                text = text[: match.start()]
+                finish_reason = "stop"
+                break
+
+        # Avoid sending the longest suffix of text which is also a prefix
+        # of a stop sequence, as it can form a stop sequence with the text
+        # generated later.
+        longest = 0
+        for s in stop:
+            for i in range(len(s), 0, -1):
+                if text.endswith(s[:i]):
+                    longest = max(i, longest)
+                    break
+
+        end = len(text) - longest
+        if end > 0:
+            output = text[:end]
+            completion_choice = CompletionChoice(
+                text=output, index=0, logprobs=None, finish_reason=None
+            )
+            completion_chunk = CompletionChunk(
+                id=str(uuid.uuid1()),
+                object="text_completion",
+                created=int(time.time()),
+                model=model,
+                choices=[completion_choice],
+            )
+            completion_usage = CompletionUsage(
+                prompt_tokens=len(tokens),
+                completion_tokens=count + 1,
+                total_tokens=count + 1 + len(tokens),
+            )
+
+            yield completion_chunk, completion_usage
+            text = text[end:]
+
+        count += 1
+        if max_new_tokens is not None and count >= max_new_tokens:
+            finish_reason = "length"
+            break
+
+    completion_choice = CompletionChoice(
+        text=text, index=0, logprobs=None, finish_reason=finish_reason
+    )
+    completion_chunk = CompletionChunk(
+        id=str(uuid.uuid1()),
+        object="text_completion",
+        created=int(time.time()),
+        model=model,
+        choices=[completion_choice],
+    )
+    completion_usage = CompletionUsage(
+        prompt_tokens=len(tokens),
+        completion_tokens=count,
+        total_tokens=count + len(tokens),
+    )
+
+    yield completion_chunk, completion_usage
