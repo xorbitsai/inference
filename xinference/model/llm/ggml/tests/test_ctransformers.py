31c4054ca5aa0ab7a0b91654e9492aa58a4a3d78
Rui Ji <ruiji@usc.edu>
2023-08-06 19:41:05 +0800

    fix ctransformers test issue2

--- xinference/model/llm/ggml/tests/test_ctransformers.py
+++ xinference/model/llm/ggml/tests/test_ctransformers.py
@@ -217,15 +217,6 @@ def test_ctransformer_init(model_spec, model_family, mock_AutoConfig_Pretrained)
         model.model_family.model_specs[0].model_file_name_template
         == model_spec.model_file_name_template
     )
-    assert (
-        model.model_family.model_specs[0].model_local_path
-        == model.model_spec.model_local_path
-    )
-    assert (
-        model.model_family.model_specs[0].model_local_path
-        == model_spec.model_local_path
-    )
-
     assert model._llm is None
 
 
