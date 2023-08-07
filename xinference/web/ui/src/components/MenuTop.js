06f3249a3f5ac449dfe5742eacf84122b63645cd
Bojun-Feng <davidfeng@uchicago.edu>
2023-08-07 17:56:11 +0800

    add gradio theme

--- xinference/web/ui/src/components/MenuTop.js
+++ xinference/web/ui/src/components/MenuTop.js
@@ -1,25 +1,13 @@
 import { Box, IconButton, useTheme } from "@mui/material";
 import { useContext } from "react";
 import { ColorModeContext } from "../theme";
-import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
-import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
 import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
 
 const MenuTop = () => {
-  const theme = useTheme();
-  const colorMode = useContext(ColorModeContext);
-
   return (
     <Box display="flex" justifyContent="right" p={2}>
       {/* Settings */}
       <Box display="flex">
-        <IconButton onClick={colorMode.toggleColorMode}>
-          {theme.palette.mode === "light" ? (
-            <DarkModeOutlinedIcon />
-          ) : (
-            <LightModeOutlinedIcon />
-          )}
-        </IconButton>
         <IconButton>
           <NotificationsOutlinedIcon />
         </IconButton>
