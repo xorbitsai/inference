8a325682704e6346119ee35f23985476f965bec7
Bojun-Feng <davidfeng@uchicago.edu>
2023-08-10 14:59:34 +0800

    edit theme

--- xinference/web/ui/src/App.js
+++ xinference/web/ui/src/App.js
@@ -1,21 +1,26 @@
-import { CssBaseline } from "@mui/material";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 import { HashRouter, Route, Routes } from "react-router-dom";
 import { ApiContextProvider } from "./components/apiContext";
+import { useMode } from "./theme";
 import Layout from "./scenes/_layout";
+
 import RunningModels from "./scenes/running_models";
 
 function App() {
+  const [theme] = useMode();
   return (
     <div className="app">
       <HashRouter>
-        <ApiContextProvider>
-          <CssBaseline />
-          <Routes>
-            <Route element={<Layout />}>
-              <Route path="/" element={<RunningModels />} />
-            </Route>
-          </Routes>
-        </ApiContextProvider>
+        <ThemeProvider theme={theme}>
+          <ApiContextProvider>
+            <CssBaseline />
+            <Routes>
+              <Route element={<Layout />}>
+                <Route path="/" element={<RunningModels />} />
+              </Route>
+            </Routes>
+          </ApiContextProvider>
+        </ThemeProvider>
       </HashRouter>
     </div>
   );
