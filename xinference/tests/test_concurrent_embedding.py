70a72f44109a6c16abc846feba1b000248474ea3
Jiayi Ni <nijiayi1119@outlook.com>
2023-08-14 15:23:10 +0800

    fix

--- xinference/tests/test_concurrent_embedding.py
+++ xinference/tests/test_concurrent_embedding.py
@@ -1,35 +1,21 @@
-# import pytest
-
-# from ..client import ChatModelHandle, Client, RESTfulChatModelHandle, RESTfulClient
-
-
-# def test_concurrent_embedding(setup):
-#     endpoint, _ = setup
-#     client = Client(endpoint)
-#     assert len(client.list_models()) == 0
-
-#     model_uid = client.launch_model(
-#         model_name="orca", model_size_in_billions=3, quantization="q4_0"
-#     )
-#     assert len(client.list_models()) == 1
-
-#     model = client.get_model(model_uid=model_uid)
-#     assert isinstance(model, ChatModelHandle)
-
-#     completion = model.chat("write a poem.")
-#     assert "content" in completion["choices"][0]["message"]
-
-#     client.terminate_model(model_uid=model_uid)
-#     assert len(client.list_models()) == 0
-
-#     # concurrent embedding for pytorch models
+# Copyright 2022-2023 XProbe Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 
-#     model_uid = client.launch_model(
-#         model_name="orca",
-#         model_size_in_billions=3,
-#         quantization="q4_0",
-#     )
+"""
+Simple test for multithreaded embedding creation
+"""
 
 import threading
 import time
@@ -39,13 +25,11 @@ from xinference.client import RESTfulClient
 
 def embedding_thread(model, text):
     model.create_embedding(text)
-    # print(f"Embedding: {embedding}")
 
 
 def nonconcurrent_embedding(model, texts):
     for text in texts:
         model.create_embedding(text)
-        # print(f"Embedding: {embedding}")
 
 
 def main():
