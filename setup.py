1fca9eae649822000da55a23cf04a364d42d292a
Uranus <109661872+UranusSeven@users.noreply.github.com>
2023-08-17 16:29:29 +0800

    Revert "FEAT : Add Model Dashboard" (#362)

--- setup.py
+++ setup.py
@@ -15,16 +15,10 @@
 import os
 import platform
 import sys
-import shutil
-import subprocess
-import warnings
 from sysconfig import get_config_vars
 
 from pkg_resources import parse_version
-from setuptools import Command, setup
-from setuptools.command.develop import develop
-from setuptools.command.install import install
-from setuptools.command.sdist import sdist
+from setuptools import setup
 
 # From https://github.com/pandas-dev/pandas/pull/24274:
 # For mac, ensure extensions are built for macos 10.9 when compiling on a
@@ -49,77 +43,6 @@ if sys.platform == "darwin":
 repo_root = os.path.dirname(os.path.abspath(__file__))
 os.chdir(repo_root)
 
-
-class ExtraCommandMixin:
-    _extra_pre_commands = []
-
-    def run(self):
-        [self.run_command(cmd) for cmd in self._extra_pre_commands]
-        super().run()
-
-    @classmethod
-    def register_pre_command(cls, cmd):
-        cls._extra_pre_commands.append(cmd)
-
-
-class CustomInstall(ExtraCommandMixin, install):
-    pass
-
-
-class CustomDevelop(ExtraCommandMixin, develop):
-    pass
-
-
-class CustomSDist(ExtraCommandMixin, sdist):
-    pass
-
-class BuildWeb(Command):
-    """build_web command"""
-
-    user_options = []
-    _web_src_path = "xinference/web/ui"
-    _web_dest_path = "xinference/web/ui/build/index.html"
-    _commands = [
-        ["npm", "install"],
-        ["npm", "run", "build"],
-    ]
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    @classmethod
-    def run(cls):
-        if int(os.environ.get("NO_WEB_UI", "0")):
-            return
-
-        npm_path = shutil.which("npm")
-        web_src_path = os.path.join(repo_root, *cls._web_src_path.split("/"))
-        web_dest_path = os.path.join(repo_root, *cls._web_dest_path.split("/"))
-
-        if npm_path is None:
-            warnings.warn("Cannot find NPM, may affect displaying Xinference web UI")
-            return
-        else:
-            replacements = {"npm": npm_path}
-            cmd_errored = False
-            for cmd in cls._commands:
-                cmd = [replacements.get(c, c) for c in cmd]
-                proc_result = subprocess.run(cmd, cwd=web_src_path)
-                if proc_result.returncode != 0:
-                    warnings.warn(f'Failed when running `{" ".join(cmd)}`')
-                    cmd_errored = True
-                    break
-            if not cmd_errored:
-                assert os.path.exists(web_dest_path)
-
-
-CustomInstall.register_pre_command("build_web")
-CustomDevelop.register_pre_command("build_web")
-CustomSDist.register_pre_command("build_web")
-
 sys.path.append(repo_root)
 versioneer = __import__("versioneer")
 
@@ -134,14 +57,7 @@ def build_long_description():
 
 setup_options = dict(
     version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass(
-        {
-            "build_web": BuildWeb,
-            "install": CustomInstall,
-            "develop": CustomDevelop,
-            "sdist": CustomSDist,
-        }
-    ),
+    cmdclass=versioneer.get_cmdclass(),
     long_description=build_long_description(),
     long_description_content_type="text/markdown",
 )
