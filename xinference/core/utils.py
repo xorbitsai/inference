eecc896ea88a90723095d3714a49c659817f0c83
codingl2k1 <codingl2k1@outlook.com>
2023-09-01 10:33:20 +0800

    Fix

--- xinference/core/utils.py
+++ xinference/core/utils.py
@@ -69,6 +69,8 @@ def parse_replica_model_uid(replica_model_uid: str) -> Tuple[str, int, int]:
     Parse replica model uid to model uid, replica and rep id.
     """
     parts = replica_model_uid.split("-")
+    if len(parts) == 1:
+        return replica_model_uid, -1, -1
     rep_id = int(parts.pop())
     replica = int(parts.pop())
     model_uid = "-".join(parts)
