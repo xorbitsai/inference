230884b13821cb5254fbeb340201c943ac2ddccc
codingl2k1 <codingl2k1@outlook.com>
2023-10-11 15:56:13 +0800

    Fix lint

--- xinference/core/tests/test_restful_api.py
+++ xinference/core/tests/test_restful_api.py
@@ -12,9 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import openai
 import pytest
 import requests
-import openai
 
 from ...model.embedding import BUILTIN_EMBEDDING_MODELS
 
