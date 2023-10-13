b0b4644e4a467798c0cdab676b4b2ec71cde00aa
UranusSeven <109661872+UranusSeven@users.noreply.github.com>
2023-10-13 15:12:58 +0800

    Fix

--- xinference/core/supervisor.py
+++ xinference/core/supervisor.py
@@ -226,18 +226,18 @@ class SupervisorActor(xo.Actor):
         )
 
         try:
-            for rep_model_uid in iter_replica_model_uid(model_uid, replica):
-                yield worker_ref.launch_speculative_model(
-                    model_uid=rep_model_uid,
-                    model_name=model_name,
-                    model_size_in_billions=model_size_in_billions,
-                    quantization=quantization,
-                    draft_model_name=draft_model_name,
-                    draft_model_size_in_billions=draft_model_size_in_billions,
-                    draft_quantization=draft_quantization,
-                    n_gpu=n_gpu,
-                )
-                self._replica_model_uid_to_worker[rep_model_uid] = worker_ref
+            rep_model_uid = f"{model_uid}-{1}-{0}"
+            yield worker_ref.launch_speculative_model(
+                model_uid=rep_model_uid,
+                model_name=model_name,
+                model_size_in_billions=model_size_in_billions,
+                quantization=quantization,
+                draft_model_name=draft_model_name,
+                draft_model_size_in_billions=draft_model_size_in_billions,
+                draft_quantization=draft_quantization,
+                n_gpu=n_gpu,
+            )
+            self._replica_model_uid_to_worker[rep_model_uid] = worker_ref
 
         except Exception:
             # terminate_model will remove the replica info.
