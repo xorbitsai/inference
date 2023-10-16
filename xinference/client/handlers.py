027318ba562b2c234fd2ebc9b5cf9f69ba277ee6
codingl2k1 <codingl2k1@outlook.com>
2023-10-16 16:37:08 +0800

    Fix lint

--- xinference/client/handlers.py
+++ xinference/client/handlers.py
@@ -1,7 +1,15 @@
 from .restful.restful_client import (
-    RESTfulChatglmCppChatModelHandle as ChatglmCppChatModelHandle,
+    RESTfulChatglmCppChatModelHandle as ChatglmCppChatModelHandle,  # noqa: F401
 )
-from .restful.restful_client import RESTfulChatModelHandle as ChatModelHandle
-from .restful.restful_client import RESTfulEmbeddingModelHandle as EmbeddingModelHandle
-from .restful.restful_client import RESTfulGenerateModelHandle as GenerateModelHandle
-from .restful.restful_client import RESTfulImageModelHandle as ImageModelHandle
+from .restful.restful_client import (
+    RESTfulChatModelHandle as ChatModelHandle,  # noqa: F401
+)
+from .restful.restful_client import (
+    RESTfulEmbeddingModelHandle as EmbeddingModelHandle,  # noqa: F401
+)
+from .restful.restful_client import (
+    RESTfulGenerateModelHandle as GenerateModelHandle,  # noqa: F401
+)
+from .restful.restful_client import (
+    RESTfulImageModelHandle as ImageModelHandle,  # noqa: F401
+)
