651c321d5219f70f15af6481b8d70a027f0a7498
ChengjieLi <chengjieli23@outlook.com>
2023-12-05 15:45:06 +0800

    rebase

--- xinference/web/ui/src/components/fetcher.js
+++ xinference/web/ui/src/components/fetcher.js
@@ -1,16 +1,16 @@
 // Some users use https proxies for backend service such as nginx.
 // So if the url has a https prefix, add a special header to avoid `Mixed Content` issue.
 function updateOptions(url, options) {
-  const update = { ...options };
-  if (url.startsWith("https://")) {
+  const update = { ...options }
+  if (url.startsWith('https://')) {
     update.headers = {
       ...update.headers,
-      "Content-Security-Policy": "upgrade-insecure-requests",
-    };
+      'Content-Security-Policy': 'upgrade-insecure-requests',
+    }
   }
-  return update;
+  return update
 }
 
 export default function fetcher(url, options) {
-  return fetch(url, updateOptions(url, options));
+  return fetch(url, updateOptions(url, options))
 }
