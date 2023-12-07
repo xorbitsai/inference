8312725f7b4a1fb05aba57759d662c2b0fb887c6
codingl2k1 <codingl2k1@outlook.com>
2023-12-07 16:52:47 +0800

    Fix

--- xinference/types.py
+++ xinference/types.py
@@ -307,10 +307,10 @@ def fix_forward_ref(model):
         if isinstance(field.annotation, ForwardRef):
             exclude_fields.append(key)
             include_fields[key] = (Optional[Any], None)
-    if exclude_fields is not None:
+    if exclude_fields:
         for key in exclude_fields:
-            model.__fields__.pop(key)
-    if include_fields is not None:
+            model.__fields__.pop(key, None)
+    if include_fields:
         dummy_model = create_model("DummyModel", **include_fields)
         model.__fields__.update(dummy_model.__fields__)
     return model
