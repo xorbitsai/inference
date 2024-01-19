4d8d38b16d24bfccb086a46ec6eff63da28acf23
aresnow <aresnow1@gmail.com>
2024-01-19 16:10:09 +0800

    Use origin implementation

--- xinference/model/llm/pytorch/core.py
+++ xinference/model/llm/pytorch/core.py
@@ -192,7 +192,8 @@ class PytorchModel(LLM):
     ) -> bool:
         if llm_spec.model_format not in ["pytorch", "gptq"]:
             return False
-        if llm_family.model_name in [
+        model_family = llm_family.model_family or llm_family.model_name
+        if model_family in [
             "baichuan-chat",
             "vicuna-v1.3",
             "falcon",
