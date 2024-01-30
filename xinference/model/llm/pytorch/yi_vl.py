50b94c1574b0cb94f8d003b7b0d6e3aff180f13d
codingl2k1 <codingl2k1@outlook.com>
2024-01-30 15:01:28 +0800

    dev

--- xinference/model/llm/pytorch/yi_vl.py
+++ xinference/model/llm/pytorch/yi_vl.py
@@ -13,11 +13,16 @@
 # limitations under the License.
 import base64
 import logging
-import operator
-import tempfile
 import time
 import uuid
+from concurrent.futures import ThreadPoolExecutor
+from io import BytesIO
 from typing import Dict, Iterator, List, Optional, Union
+
+import requests
+import torch
+from PIL import Image
+from transformers import TextIteratorStreamer
 
 from ....model.utils import select_device
 from ....types import (
@@ -33,47 +38,33 @@ from .core import PytorchChatModel, PytorchGenerateConfig
 logger = logging.getLogger(__name__)
 
 
-class QwenVLChatModel(PytorchChatModel):
+class YiVLChatModel(PytorchChatModel):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self._tokenizer = None
         self._model = None
+        self._image_processor = None
 
     @classmethod
     def match(
         cls, model_family: "LLMFamilyV1", model_spec: "LLMSpecV1", quantization: str
     ) -> bool:
-        if "qwen" in model_family.model_name:
+        if "yi" in model_family.model_name:
             return True
         return False
 
     def load(self):
-        from transformers import AutoModelForCausalLM, AutoTokenizer
-        from transformers.generation import GenerationConfig
+        from ....thirdparty.llava.mm_utils import load_pretrained_model
 
-        device = self._pytorch_model_config.get("device", "auto")
-        device = select_device(device)
+        (
+            self._tokenizer,
+            self._model,
+            self._image_processor,
+            _,
+        ) = load_pretrained_model(self.model_path)
 
-        self._tokenizer = AutoTokenizer.from_pretrained(
-            self.model_path,
-            trust_remote_code=True,
-            code_revision=self.model_spec.model_revision,
-        )
-        self._model = AutoModelForCausalLM.from_pretrained(
-            self.model_path,
-            device_map=device,
-            trust_remote_code=True,
-            code_revision=self.model_spec.model_revision,
-        ).eval()
-        # Specify hyperparameters for generation
-        self._model.generation_config = GenerationConfig.from_pretrained(
-            self.model_path,
-            trust_remote_code=True,
-            code_revision=self.model_spec.model_revision,
-        )
-
-    def _message_content_to_qwen(self, content) -> str:
-        def _ensure_url(_url):
+    def _message_content_to_yi(self, content) -> Union[str, tuple]:
+        def _load_image(_url):
             if _url.startswith("data:"):
                 logging.info("Parse url by base64 decoder.")
                 # https://platform.openai.com/docs/guides/vision/uploading-base-64-encoded-images
@@ -83,25 +74,37 @@ class QwenVLChatModel(PytorchChatModel):
                 data = data[len("base64,") :]
                 data = base64.b64decode(data.encode("utf-8"))
 
-                with tempfile.NamedTemporaryFile(suffix=f".{ext}", delete=False) as f:
-                    f.write(data)
-                logging.info("Dump base64 data to %s", f.name)
-                return f.name
+                return Image.open(BytesIO(data))
             else:
-                if len(_url) > 2048:
-                    raise Exception(f"Image url is too long, {len(_url)} > 2048.")
-                return _url
+                response = requests.get(_url)
+                return Image.open(BytesIO(response.content))
 
         if not isinstance(content, str):
-            # TODO(codingl2k1): Optimize _ensure_url
-            content = [
-                {"image": _ensure_url(c["image_url"]["url"]), "type": "image"}
-                if c.get("type") == "image_url"
-                else c
-                for c in content
-            ]
-            content = sorted(content, key=operator.itemgetter("type"))
-            return self._tokenizer.from_list_format(content)
+            from ....thirdparty.llava.model.constants import DEFAULT_IMAGE_TOKEN
+
+            texts = []
+            image_urls = []
+            for c in content:
+                c_type = c.get("type")
+                if c_type == "text":
+                    texts.append(c["text"])
+                elif c_type == "image_url":
+                    image_urls.append(c["image_url"])
+            image_futures = []
+            with ThreadPoolExecutor() as executor:
+                for image_url in image_urls:
+                    fut = executor.submit(_load_image, image_url)
+                    image_futures.append(fut)
+            images = [fut.result() for fut in image_futures]
+            text = " ".join(texts)
+            if DEFAULT_IMAGE_TOKEN not in text:
+                text = DEFAULT_IMAGE_TOKEN + "\n" + text
+            if len(images) == 0:
+                return text
+            elif len(images) == 1:
+                return text, images[0], "Pad"
+            else:
+                raise RuntimeError("Only one image per message is supported by Yi VL.")
         return content
 
     def chat(
@@ -115,23 +118,69 @@ class QwenVLChatModel(PytorchChatModel):
             raise Exception(
                 f"Chat with model {self.model_family.model_name} does not support stream."
             )
-        prompt = self._message_content_to_qwen(prompt)
-        # Convert openai history to qwen vl history
-        qwen_history = []
-        query_to_response: List = []
-        for h in chat_history or []:
-            role = h["role"]
-            content = self._message_content_to_qwen(h["content"])
-            if len(query_to_response) == 0 and role == "user":
-                query_to_response.append(content)
-            if len(query_to_response) == 1 and role == "assistant":
-                query_to_response.append(content)
-            if len(query_to_response) == 2:
-                qwen_history.append(query_to_response)
-                query_to_response = []
-        response, history = self._model.chat(
-            self._tokenizer, query=prompt, history=qwen_history
+        from ....thirdparty.llava.conversation import conv_templates
+        from ....thirdparty.llava.mm_utils import (
+            KeywordsStoppingCriteria,
+            tokenizer_image_token,
         )
+        from ....thirdparty.llava.model.constants import IMAGE_TOKEN_INDEX
+
+        # Convert chat history to llava state
+        state = conv_templates["mm_default"].copy()
+        for message in chat_history or []:
+            content = self._message_content_to_yi(message["content"])
+            state.append_message(message["role"], content)
+        state.append_message(state.roles[0], self._message_content_to_yi(prompt))
+        state.append_message(state.roles[1], None)
+
+        prompt = state.get_prompt()
+
+        input_ids = (
+            tokenizer_image_token(
+                prompt, self._tokenizer, IMAGE_TOKEN_INDEX, return_tensors="pt"
+            )
+            .unsqueeze(0)
+            .cuda()
+        )
+
+        images = state.get_images(return_pil=True)
+        image = images[0]
+
+        image_tensor = self._image_processor.preprocess(image, return_tensors="pt")[
+            "pixel_values"
+        ][0]
+
+        stop_str = state.sep
+        keywords = [stop_str]
+        stopping_criteria = KeywordsStoppingCriteria(
+            keywords, self._tokenizer, input_ids
+        )
+        streamer = TextIteratorStreamer(
+            self._tokenizer, timeout=60, skip_prompt=True, skip_special_tokens=True
+        )
+        top_p = generate_config.get("top_p", 0.7)
+        temperature = generate_config.get("temperature", 0.2)
+        max_new_tokens = generate_config.get("max_tokens", 512)
+        generate_kwargs = {
+            "input_ids": input_ids,
+            "images": image_tensor.unsqueeze(0).to(dtype=torch.bfloat16).cuda(),
+            "streamer": streamer,
+            "do_sample": True,
+            "top_p": float(top_p),
+            "temperature": float(temperature),
+            "stopping_criteria": [stopping_criteria],
+            "use_cache": True,
+            "max_new_tokens": min(int(max_new_tokens), 1536),
+        }
+        r = self._model.generate(**generate_kwargs)
+        print("fffffffffffffff", r)
+        # from threading import Thread
+        # t = Thread(target=model.generate, kwargs=generate_kwargs)
+        # t.start()
+
+        # response, history = self._model.chat(
+        #     self._tokenizer, query=prompt, history=qwen_history
+        # )
         return ChatCompletion(
             id="chat" + str(uuid.uuid1()),
             object="chat.completion",
@@ -140,7 +189,7 @@ class QwenVLChatModel(PytorchChatModel):
             choices=[
                 ChatCompletionChoice(
                     index=0,
-                    message={"role": "assistant", "content": response},
+                    message={"role": "assistant", "content": r},
                     finish_reason="stop",
                 )
             ],
