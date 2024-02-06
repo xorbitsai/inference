70567afd40a720e2f76ecd3912871bd334401fa0
codingl2k1 <codingl2k1@outlook.com>
2024-02-06 12:08:56 +0800

    Fix function call

--- xinference/model/llm/utils.py
+++ xinference/model/llm/utils.py
@@ -556,7 +556,7 @@ Begin!"""
             content, func, args = cls._eval_gorilla_openfunctions_arguments(c, tools)
         elif "chatglm3" == family:
             content, func, args = cls._eval_chatglm3_arguments(c, tools)
-        elif "qwen-chat" == family:
+        elif family in ["qwen-chat", "qwen1.5-chat"]:
             content, func, args = cls._eval_qwen_chat_arguments(c, tools)
         else:
             raise Exception(
