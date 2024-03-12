8339d1108894e1147e44fecc116bec682b8c87ca
ChengjieLi <chengjieli23@outlook.com>
2024-03-12 18:26:07 +0800

    fix

--- xinference/web/ui/src/scenes/launch_model/rerankCard.js
+++ xinference/web/ui/src/scenes/launch_model/rerankCard.js
@@ -1,10 +1,17 @@
-import {RocketLaunchOutlined, UndoOutlined} from '@mui/icons-material'
+import { RocketLaunchOutlined, UndoOutlined } from '@mui/icons-material'
 import DeleteIcon from '@mui/icons-material/Delete'
-import {Box, Chip, CircularProgress, FormControl, Stack, TextField,} from '@mui/material'
+import {
+  Box,
+  Chip,
+  CircularProgress,
+  FormControl,
+  Stack,
+  TextField,
+} from '@mui/material'
 import IconButton from '@mui/material/IconButton'
-import React, {useContext, useEffect, useState} from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 
-import {ApiContext} from '../../components/apiContext'
+import { ApiContext } from '../../components/apiContext'
 import fetcher from '../../components/fetcher'
 
 const CARD_HEIGHT = 270
