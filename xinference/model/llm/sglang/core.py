9cbc5dabe04542d1d797668453c6b57c6a118a29
ChengjieLi <chengjieli23@outlook.com>
2024-03-18 16:22:29 +0800

    dev

--- xinference/model/llm/sglang/core.py
+++ xinference/model/llm/sglang/core.py
@@ -15,17 +15,13 @@
 import logging
 import time
 import uuid
-from typing import (  # TYPE_CHECKING,; Any,; Iterable,
-    AsyncGenerator,
-    Dict,
-    List,
-    Optional,
-    TypedDict,
-    Union,
-)
+from typing import AsyncGenerator, Dict, List, Optional, TypedDict, Union
 
 from ....constants import XINFERENCE_ENABLE_SGLANG
-from ....types import (  # ChatCompletion,; ChatCompletionChunk,; ChatCompletionMessage,
+from ....types import (
+    ChatCompletion,
+    ChatCompletionChunk,
+    ChatCompletionMessage,
     Completion,
     CompletionChoice,
     CompletionChunk,
@@ -33,8 +29,7 @@ from ....types import (  # ChatCompletion,; ChatCompletionChunk,; ChatCompletion
 )
 from .. import LLM, LLMFamilyV1, LLMSpecV1
 from ..llm_family import CustomLLMFamilyV1
-
-# from ..utils import ChatModelMixin
+from ..utils import ChatModelMixin
 
 logger = logging.getLogger(__name__)
 
@@ -45,6 +40,7 @@ class SGLANGModelConfig(TypedDict, total=False):
     tp_size: int
     mem_fraction_static: float
     log_level: str
+    attention_reduce_in_fp32: bool  # For gemma
 
 
 class SGLANGGenerateConfig(TypedDict, total=False):
@@ -66,28 +62,16 @@ try:
 except ImportError:
     SGLANG_INSTALLED = False
 
-SGLANG_SUPPORTED_MODELS = ["llama-2", "qwen-chat", "qwen1.5-chat"]
-# VLLM_SUPPORTED_CHAT_MODELS = [
-#     "llama-2-chat",
-#     "vicuna-v1.3",
-#     "vicuna-v1.5",
-#     "baichuan-chat",
-#     "internlm-chat-7b",
-#     "internlm-chat-8k",
-#     "internlm-chat-20b",
-#     "qwen-chat",
-#     "Yi",
-#     "Yi-chat",
-#     "code-llama",
-#     "code-llama-python",
-#     "code-llama-instruct",
-#     "mistral-instruct-v0.1",
-#     "mistral-instruct-v0.2",
-#     "mixtral-instruct-v0.1",
-#     "chatglm3",
-# ]
-# if VLLM_INSTALLED and vllm.__version__ >= "0.3.0":
-#     VLLM_SUPPORTED_CHAT_MODELS.append("qwen1.5-chat")
+SGLANG_SUPPORTED_MODELS = ["llama-2", "mistral-v0.1", "mixtral-v0.1"]
+SGLANG_SUPPORTED_CHAT_MODELS = [
+    "llama-2-chat",
+    "qwen-chat",
+    "qwen1.5-chat",
+    "mistral-instruct-v0.1",
+    "mistral-instruct-v0.2",
+    "mixtral-instruct-v0.1",
+    "gemma-it",
+]
 
 
 class SGLANGModel(LLM):
@@ -139,6 +123,7 @@ class SGLANGModel(LLM):
         model_config.setdefault("tp_size", cuda_count)
         model_config.setdefault("mem_fraction_static", 0.9)
         model_config.setdefault("log_level", "info")
+        model_config.setdefault("attention_reduce_in_fp32", False)
 
         return model_config
 
@@ -156,7 +141,9 @@ class SGLANGModel(LLM):
         generate_config.setdefault("top_k", -1)
         # See https://github.com/sgl-project/sglang/blob/main/python/sglang/lang/ir.py#L120
         # 16 is too less, so here set 256 by default
-        generate_config.setdefault("max_new_tokens", 256)
+        generate_config.setdefault(
+            "max_new_tokens", generate_config.pop("max_tokens", 256)  # type: ignore
+        )
         generate_config.setdefault("stop", [])
         generate_config.setdefault("stream", False)
         generate_config.setdefault("ignore_eos", False)
@@ -188,8 +175,8 @@ class SGLANGModel(LLM):
         else:
             if llm_family.model_name not in SGLANG_SUPPORTED_MODELS:
                 return False
-        # if "generate" not in llm_family.model_ability:
-        #     return False
+        if "generate" not in llm_family.model_ability:
+            return False
         return SGLANG_INSTALLED
 
     @staticmethod
@@ -302,89 +289,66 @@ class SGLANGModel(LLM):
             return stream_results()
 
 
-# class VLLMChatModel(VLLMModel, ChatModelMixin):
-#     @classmethod
-#     def match(
-#         cls, llm_family: "LLMFamilyV1", llm_spec: "LLMSpecV1", quantization: str
-#     ) -> bool:
-#         if XINFERENCE_DISABLE_VLLM:
-#             return False
-#         if llm_spec.model_format not in ["pytorch", "gptq", "awq"]:
-#             return False
-#         if llm_spec.model_format == "pytorch":
-#             if quantization != "none" and not (quantization is None):
-#                 return False
-#         if llm_spec.model_format in ["gptq", "awq"]:
-#             # Currently, only 4-bit weight quantization is supported for GPTQ, but got 8 bits.
-#             if "4" not in quantization:
-#                 return False
-#         if isinstance(llm_family, CustomLLMFamilyV1):
-#             if llm_family.model_family not in VLLM_SUPPORTED_CHAT_MODELS:
-#                 return False
-#         else:
-#             if llm_family.model_name not in VLLM_SUPPORTED_CHAT_MODELS:
-#                 return False
-#         if "chat" not in llm_family.model_ability:
-#             return False
-#         return VLLM_INSTALLED
-#
-#     def _sanitize_chat_config(
-#         self,
-#         generate_config: Optional[Dict] = None,
-#     ) -> Dict:
-#         if not generate_config:
-#             generate_config = {}
-#         if self.model_family.prompt_style:
-#             if (
-#                 not generate_config.get("stop")
-#             ) and self.model_family.prompt_style.stop:
-#                 generate_config["stop"] = self.model_family.prompt_style.stop.copy()
-#             if self.model_family.prompt_style.stop_token_ids:
-#                 generate_config.setdefault(
-#                     "stop_token_ids",
-#                     self.model_family.prompt_style.stop_token_ids.copy(),
-#                 )
-#         return generate_config
-#
-#     async def async_chat(
-#         self,
-#         prompt: str,
-#         system_prompt: Optional[str] = None,
-#         chat_history: Optional[List[ChatCompletionMessage]] = None,
-#         generate_config: Optional[Dict] = None,
-#     ) -> Union[ChatCompletion, AsyncGenerator[ChatCompletionChunk, None]]:
-#         assert self.model_family.prompt_style is not None
-#         prompt_style = self.model_family.prompt_style.copy()
-#         if system_prompt:
-#             prompt_style.system_prompt = system_prompt
-#         chat_history = chat_history or []
-#         tools = generate_config.pop("tools", []) if generate_config else None
-#         full_prompt = self.get_prompt(prompt, chat_history, prompt_style, tools=tools)
-#
-#         generate_config = self._sanitize_chat_config(generate_config)
-#         # TODO(codingl2k1): qwen hacky to set stop for function call.
-#         model_family = self.model_family.model_family or self.model_family.model_name
-#         if tools and "qwen-chat" == model_family:
-#             stop = generate_config.get("stop")
-#             if isinstance(stop, str):
-#                 generate_config["stop"] = [stop, "Observation:"]
-#             elif isinstance(stop, Iterable):
-#                 assert not isinstance(stop, str)
-#                 generate_config["stop"] = list(stop) + ["Observation:"]
-#             else:
-#                 generate_config["stop"] = "Observation:"
-#
-#         stream = generate_config.get("stream", None)
-#
-#         if stream:
-#             agen = await self.async_generate(full_prompt, generate_config)
-#             assert isinstance(agen, AsyncGenerator)
-#             return self._async_to_chat_completion_chunks(agen)
-#         else:
-#             c = await self.async_generate(full_prompt, generate_config)
-#             assert not isinstance(c, AsyncGenerator)
-#             if tools:
-#                 return self._tool_calls_completion(
-#                     self.model_family, self.model_uid, c, tools
-#                 )
-#             return self._to_chat_completion(c)
+class SGLANGChatModel(SGLANGModel, ChatModelMixin):
+    @classmethod
+    def match(
+        cls, llm_family: "LLMFamilyV1", llm_spec: "LLMSpecV1", quantization: str
+    ) -> bool:
+        if not XINFERENCE_ENABLE_SGLANG:
+            return False
+        if llm_spec.model_format not in ["pytorch", "gptq", "awq"]:
+            return False
+        if llm_spec.model_format == "pytorch":
+            if quantization != "none" and not (quantization is None):
+                return False
+        if llm_spec.model_format in ["gptq", "awq"]:
+            # Currently, only 4-bit weight quantization is supported for GPTQ, but got 8 bits.
+            if "4" not in quantization:
+                return False
+        if isinstance(llm_family, CustomLLMFamilyV1):
+            if llm_family.model_family not in SGLANG_SUPPORTED_CHAT_MODELS:
+                return False
+        else:
+            if llm_family.model_name not in SGLANG_SUPPORTED_CHAT_MODELS:
+                return False
+        if "chat" not in llm_family.model_ability:
+            return False
+        return SGLANG_INSTALLED
+
+    def _sanitize_chat_config(
+        self,
+        generate_config: Optional[Dict] = None,
+    ) -> Dict:
+        if not generate_config:
+            generate_config = {}
+        if self.model_family.prompt_style:
+            if (
+                not generate_config.get("stop")
+            ) and self.model_family.prompt_style.stop:
+                generate_config["stop"] = self.model_family.prompt_style.stop.copy()
+        return generate_config
+
+    async def async_chat(
+        self,
+        prompt: str,
+        system_prompt: Optional[str] = None,
+        chat_history: Optional[List[ChatCompletionMessage]] = None,
+        generate_config: Optional[Dict] = None,
+    ) -> Union[ChatCompletion, AsyncGenerator[ChatCompletionChunk, None]]:
+        assert self.model_family.prompt_style is not None
+        prompt_style = self.model_family.prompt_style.copy()
+        if system_prompt:
+            prompt_style.system_prompt = system_prompt
+        chat_history = chat_history or []
+        full_prompt = self.get_prompt(prompt, chat_history, prompt_style)
+
+        generate_config = self._sanitize_chat_config(generate_config)
+        stream = generate_config.get("stream", None)
+        if stream:
+            agen = await self.async_generate(full_prompt, generate_config)  # type: ignore
+            assert isinstance(agen, AsyncGenerator)
+            return self._async_to_chat_completion_chunks(agen)
+        else:
+            c = await self.async_generate(full_prompt, generate_config)  # type: ignore
+            assert not isinstance(c, AsyncGenerator)
+            return self._to_chat_completion(c)
