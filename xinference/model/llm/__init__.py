2c9465ade7f358d57d4bc087277882d896a8de15
Chengjie Li <109656400+ChengjieLi28@users.noreply.github.com>
2024-03-21 14:58:01 +0800

    FEAT: Supports `sglang` backend (#1161)

--- xinference/model/llm/__init__.py
+++ xinference/model/llm/__init__.py
@@ -60,6 +60,7 @@ def _install():
     from .pytorch.qwen_vl import QwenVLChatModel
     from .pytorch.vicuna import VicunaPytorchChatModel
     from .pytorch.yi_vl import YiVLChatModel
+    from .sglang.core import SGLANGChatModel, SGLANGModel
     from .vllm.core import VLLMChatModel, VLLMModel
 
     # register llm classes.
@@ -79,6 +80,7 @@ def _install():
             CtransformersModel,
         ]
     )
+    LLM_CLASSES.extend([SGLANGModel, SGLANGChatModel])
     LLM_CLASSES.extend([VLLMModel, VLLMChatModel])
     LLM_CLASSES.extend(
         [
