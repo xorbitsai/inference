ab784a87ff7fc96b34dd014bab549d1b8ba4ac8c
yiboyasss <3359595624@qq.com>
2024-04-17 14:43:00 +0800

    detail

--- xinference/web/ui/src/scenes/register_model/register_llm.js
+++ xinference/web/ui/src/scenes/register_model/register_llm.js
@@ -1,33 +1,51 @@
 import './styles/registerLLMStyle.css'
 
+import AddIcon from '@mui/icons-material/Add';
+import DeleteIcon from '@mui/icons-material/Delete'
+import FilterNoneIcon from '@mui/icons-material/FilterNone';
 import KeyboardDoubleArrowRightIcon from '@mui/icons-material/KeyboardDoubleArrowRight';
 import NotesIcon from '@mui/icons-material/Notes';
 import {
+  Alert,
+  AlertTitle,
   Box,
+  Button,
   Checkbox,
+  Chip,
   FormControl,
   FormControlLabel,
   FormHelperText,
+  InputLabel,
+  MenuItem,
   Radio,
   RadioGroup,
+  Select,
+  Stack,
+  TextField,
+  Tooltip,
 } from '@mui/material'
-import Alert from '@mui/material/Alert'
-import AlertTitle from '@mui/material/AlertTitle'
-import Button from '@mui/material/Button'
-import TextField from '@mui/material/TextField'
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useContext, useEffect, useRef, useState } from 'react'
 import { useCookies } from 'react-cookie'
 import { useNavigate } from 'react-router-dom'
 
 import { ApiContext } from '../../components/apiContext'
 import fetcher from '../../components/fetcher'
 import { useMode } from '../../theme'
+import languages from './data/languages';
 
 const SUPPORTED_LANGUAGES_DICT = { en: 'English', zh: 'Chinese' }
-const SUPPORTED_FEATURES = ['Generate', 'Chat']
+const SUPPORTED_FEATURES = ['Generate', 'Chat', 'Vision']
 
 // Convert dictionary of supported languages into list
 const SUPPORTED_LANGUAGES = Object.keys(SUPPORTED_LANGUAGES_DICT)
+
+const modelFormatArr = [
+  {value: 'pytorch', label: 'PyTorch'},
+  {value: 'ggmlv3', label: 'GGML'},
+  {value: 'ggufv2', label: 'GGUF'},
+  {value: 'gptq', label: 'GPTQ'},
+  {value: 'awq', label: 'AWQ'},
+]
 
 const RegisterLLM = () => {
   const ERROR_COLOR = useMode()
@@ -56,6 +74,20 @@ const RegisterLLM = () => {
   })
   const [familyLabel, setFamilyLabel] = useState('')
   const [isShow, setIsShow] = useState(false)
+  const [count, setCount] = useState(1)
+  const [languagesArr, setLanguagesArr] = useState([])
+  const [specsArr, setSpecsArr] = useState([{
+    id: 0,
+    model_url: '/path/to/llama-2',
+    model_size: 7,
+    model_format: 'pytorch',
+    quantization: ''
+  }])
+  const [modelSizeAlert, setModelSizeAlert] = useState(false)
+  const [modelSizeAlertIndex, setModelSizeAlertIndex] = useState(-1)
+
+  const scrollRef = useRef(null)
+
   const [cookie] = useCookies(['token'])
   const navigate = useNavigate()
 
@@ -162,11 +194,24 @@ const RegisterLLM = () => {
   }, [cookie.token])
 
   const getFamilyByAbility = () => {
-    if (formData.model_ability.includes('chat')) {
+    if (formData.model_ability.includes('chat') || formData.model_ability.includes('vision')) {
       return family.chat
     } else {
       return family.generate
     }
+  }
+  const sortStringsByFirstLetter = (arr) => {
+    return arr.sort((a, b) => {
+      const firstCharA = a.charAt(0).toLowerCase()
+      const firstCharB = b.charAt(0).toLowerCase()
+      if (firstCharA < firstCharB) {
+        return -1
+      }
+      if (firstCharA > firstCharB) {
+        return 1
+      }
+      return 0
+   })
   }
 
   const isModelFormatPytorch = () => {
@@ -318,14 +363,80 @@ const RegisterLLM = () => {
     }
   }
 
+  const handleSelectLanguages = (value) => {
+    const arr = [...languagesArr, value]
+    setLanguagesArr(arr)
+  }
+
+  const handleDeleteLanguages = (index) => {
+    const arr = languagesArr.filter((_, subIndex) => subIndex !== index)
+    setLanguagesArr(arr)
+  }
+
+  const handleAddSpecs = () => {
+    setCount(count + 1)
+    const item = {
+      id: count,
+      model_url: '/path/to/llama-2',
+      model_size: 7,
+      model_format: 'pytorch',
+      quantization: ''
+    }
+    setSpecsArr([...specsArr, item])
+
+    scrollRef.current.scrollTo({
+      top: scrollRef.current.scrollHeight,
+      behavior: 'smooth',
+    })
+  }
+
+  const handleUpdateSpecsArr = (index, type, newValue) => {
+    console.log('handleUpdateSpecsArr', index, type, newValue);
+    setSpecsArr(
+      specsArr.map((item, subIndex) => {
+        if (subIndex === index) {
+          return { ...item, [type]: newValue }
+        }
+        return item
+      })
+    )
+  }
+
+  const handleDeleteSpecs = (id) => {
+    console.log('删除id:', id);
+  }
+
+  const handleModelSize = (index, value) => {
+    setModelSizeAlert(false)
+    
+    if(Number(value)) { 
+      parseInt(value) === parseFloat(value) ? value = Number(value) : value = value.split('.').join('_')
+    }else {
+      setModelSizeAlert(true)
+      setModelSizeAlertIndex(index)
+    }
+    handleUpdateSpecsArr(index, 'model_size', value)
+    setModelSize(Number(value))
+  }
+
+  const handleChangeText = (value) => {
+    console.log('text-value', value);
+  }
+
+  const handleCopy = () => {
+    console.log('复制');
+  }
+
   return (
     <Box style={{display: 'flex'}}>
-      <div className='formBox'>
-        {isShow ? <KeyboardDoubleArrowRightIcon className='icon arrow' onClick={() => setIsShow(!isShow)} />
-        : <NotesIcon className='icon notes' onClick={() => setIsShow(!isShow)} /> }
+      <div ref={scrollRef} className={isShow ? 'formBox' : ' show formBox' }>
+        {isShow ? 
+        <Tooltip title="Pack up" placement="top"><KeyboardDoubleArrowRightIcon className='icon arrow' onClick={() => setIsShow(!isShow)} /></Tooltip>
+        : <Tooltip title="Unfold" placement="top"><NotesIcon className='icon notes' onClick={() => setIsShow(!isShow)} /></Tooltip> }
 
         {/* Base Information */}
         <FormControl sx={styles.baseFormControl}>
+          {/* name */}
           <TextField
             label="Model Name"
             error={errorModelName}
@@ -338,60 +449,22 @@ const RegisterLLM = () => {
           />
           <Box padding="15px"></Box>
 
-          <label
-            style={{
-              paddingLeft: 5,
-            }}
-          >
-            Model Format
-          </label>
-
-          <RadioGroup
-            value={modelFormat}
-            onChange={(e) => {
-              setModelFormat(e.target.value)
-            }}
-          >
-            <Box sx={styles.checkboxWrapper}>
-              <Box sx={{ marginLeft: '10px' }}>
-                <FormControlLabel
-                  value="pytorch"
-                  control={<Radio />}
-                  label="PyTorch"
-                />
-              </Box>
-              <Box sx={{ marginLeft: '10px' }}>
-                <FormControlLabel
-                  value="ggmlv3"
-                  control={<Radio />}
-                  label="GGML"
-                />
-              </Box>
-              <Box sx={{ marginLeft: '10px' }}>
-                <FormControlLabel
-                  value="ggufv2"
-                  control={<Radio />}
-                  label="GGUF"
-                />
-              </Box>
-              <Box sx={{ marginLeft: '10px' }}>
-                <FormControlLabel
-                  value="gptq"
-                  control={<Radio />}
-                  label="GPTQ"
-                />
-              </Box>
-              <Box sx={{ marginLeft: '10px' }}>
-                <FormControlLabel
-                  value="awq"
-                  control={<Radio />}
-                  label="AWQ"
-                />
-              </Box>
-            </Box>
-          </RadioGroup>
+          {/* description */}
+          <TextField
+            label="Model Description (Optional)"
+            error={errorModelDescription}
+            defaultValue={formData.model_description}
+            size="small"
+            onChange={(event) =>
+              setFormData({
+                ...formData,
+                model_description: event.target.value,
+              })
+            }
+          />
           <Box padding="15px"></Box>
 
+          {/* Context Length */}
           <TextField
             error={errorContextLength}
             label="Context Length"
@@ -416,62 +489,7 @@ const RegisterLLM = () => {
           />
           <Box padding="15px"></Box>
 
-          <TextField
-            label="Model Size in Billions"
-            size="small"
-            error={errorModelSize}
-            value={modelSize}
-            onChange={(e) => {
-              let value = e.target.value
-              // Remove leading zeros
-              if (/^0+/.test(value)) {
-                value = value.replace(/^0+/, '') || '0'
-              }
-              // Ensure it's a positive integer, if not set it to the minimum
-              if (!/^\d+$/.test(value) || parseInt(value) < 0) {
-                value = '0'
-              }
-              setModelSize(Number(value))
-            }}
-          />
-          <Box padding="15px"></Box>
-
-          <TextField
-            label="Model Path"
-            size="small"
-            value={modelUri}
-            onChange={(e) => {
-              setModelUri(e.target.value)
-            }}
-            helperText="For PyTorch, provide the model directory. For GGML/GGUF, provide the model file path."
-          />
-          <Box padding="15px"></Box>
-
-          <TextField
-            label="Quantization (Optional)"
-            size="small"
-            value={quantization}
-            onChange={(e) => {
-              setQuantization(e.target.value)
-            }}
-            helperText="For GPTQ/AWQ models, please be careful to fill in the quantization corresponding to the model you want to register."
-          />
-          <Box padding="15px"></Box>
-
-          <TextField
-            label="Model Description (Optional)"
-            error={errorModelDescription}
-            defaultValue={formData.model_description}
-            size="small"
-            onChange={(event) =>
-              setFormData({
-                ...formData,
-                model_description: event.target.value,
-              })
-            }
-          />
-          <Box padding="15px"></Box>
-
+          {/* languages */}
           <label
             style={{
               paddingLeft: 5,
@@ -509,9 +527,41 @@ const RegisterLLM = () => {
                 />
               </Box>
             ))}
+            <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+              <InputLabel>Languages</InputLabel>
+              <Select
+                value={''}
+                label="Languages"
+                onChange={(e) => handleSelectLanguages(e.target.value)}
+                MenuProps={{
+                  PaperProps: {
+                    style: { maxHeight: '20vh' },
+                  },
+                }}
+              >
+                {
+                  languages.filter(item => !languagesArr.includes(item.code)).map((item) => (
+                    <MenuItem key={item.code} value={item.code}>{item.code}</MenuItem>
+                  ))
+                }
+              </Select>
+            </FormControl>     
           </Box>
+          <Stack direction="row" spacing={1} style={{marginLeft: '10px'}}>
+            {languagesArr.map((item, index) => (
+              <Chip
+                key={item}
+                label={item}
+                variant="outlined"
+                size="small"
+                color="primary"
+                onDelete={() => handleDeleteLanguages(index)}
+              />
+            ))}
+          </Stack>
           <Box padding="15px"></Box>
 
+          {/* abilities */}
           <label
             style={{
               paddingLeft: 5,
@@ -553,35 +603,124 @@ const RegisterLLM = () => {
             ))}
           </Box>
           <Box padding="15px"></Box>
-        </FormControl>
 
-        <FormControl sx={styles.baseFormControl}>
-          <label
-            style={{
-              paddingLeft: 5,
-              color: errorAbility ? ERROR_COLOR : 'inherit',
-            }}
-          >
-            Model Family
+          {/* family */}
+          <FormControl sx={styles.baseFormControl}>
+            <label
+              style={{
+                paddingLeft: 5,
+                color: errorAbility ? ERROR_COLOR : 'inherit',
+              }}
+            >
+              Model Family
+            </label>
+            <FormHelperText>
+              Please be careful to select the family name corresponding to the
+              model you want to register. If not found, please choose `other`.
+            </FormHelperText>
+            <RadioGroup
+              value={familyLabel}
+              onChange={(e) => {
+                setFamilyLabel(e.target.value)
+              }}
+            >
+              <Box sx={styles.checkboxWrapper} style={{marginLeft: '10px' }}>
+                {sortStringsByFirstLetter(getFamilyByAbility()).map((v) => (
+                  <Box sx={{ width: '20%' }} key={v}>
+                    <FormControlLabel value={v} control={<Radio />} label={v} />
+                  </Box>
+                ))}
+              </Box>
+            </RadioGroup>
+            <Box padding="15px"></Box>
+          </FormControl>
+
+          {/* specs */}
+          <label style={{marginBottom: '30px'}}>
+            Model Specs
+            <Button variant="contained" size="small" endIcon={<AddIcon />} className='addBtn' onClick={handleAddSpecs}>add Specs</Button>
           </label>
-          <FormHelperText>
-            Please be careful to select the family name corresponding to the
-            model you want to register. If not found, please choose `other`.
-          </FormHelperText>
-          <RadioGroup
-            value={familyLabel}
-            onChange={(e) => {
-              setFamilyLabel(e.target.value)
-            }}
-          >
-            <Box sx={styles.checkboxWrapper}>
-              {getFamilyByAbility().map((v) => (
-                <Box sx={{ marginLeft: '10px' }}>
-                  <FormControlLabel value={v} control={<Radio />} label={v} />
-                </Box>
-              ))}
-            </Box>
-          </RadioGroup>
+          {JSON.stringify(specsArr)}
+          <div className='specs_container'>
+            {
+              specsArr.map((item, index) => (
+                <div className='item' key={item.id}>
+                  <TextField
+                    label="Model Path"
+                    size="small"
+                    value={item.model_url}
+                    onChange={(e) => {
+                      setModelUri(e.target.value)
+                      handleUpdateSpecsArr(index, 'model_url', e.target.value)
+                    }}
+                    helperText="For PyTorch, provide the model directory. For GGML/GGUF, provide the model file path."
+                  />
+                  <Box padding="15px"></Box>
+
+                  <TextField
+                    label="Model Size in Billions"
+                    size="small"
+                    error={errorModelSize}
+                    value={item.model_size}
+                    onChange={(e) => {
+                      handleModelSize(index, e.target.value)
+                    }}
+                  />
+                  {modelSizeAlert && modelSizeAlertIndex === index && (
+                    <Alert severity="error">
+                      Please enter a number greater than 0.
+                    </Alert>
+                  )}
+                  <Box padding="15px"></Box>
+
+                  <label
+                    style={{
+                      paddingLeft: 5,
+                    }}
+                  >
+                    Model Format
+                  </label>
+                  <RadioGroup
+                    value={item.model_format}
+                    onChange={(e) => {
+                      setModelFormat(e.target.value)
+                      handleUpdateSpecsArr(index, 'model_format', e.target.value)
+                    }}
+                  >
+                    <Box sx={styles.checkboxWrapper}>
+                      {modelFormatArr.map(item => (
+                        <Box sx={{ marginLeft: '10px' }}>
+                          <FormControlLabel
+                            key={item.value}
+                            value={item.value}
+                            control={<Radio />}
+                            label={item.label}
+                          />
+                        </Box>
+                      ))}
+                    </Box>
+                  </RadioGroup>
+                  <Box padding="15px"></Box>
+
+                  <TextField
+                    label="Quantization (Optional)"
+                    size="small"
+                    value={quantization}
+                    onChange={(e) => {
+                      setQuantization(e.target.value)
+                    }}
+                    helperText="For GPTQ/AWQ models, please be careful to fill in the quantization corresponding to the model you want to register."
+                  />
+
+                  <Tooltip title="Delete specs" placement="top">
+                    <div className='deleteBtn' onClick={() => handleDeleteSpecs(item.id)}>
+                      <DeleteIcon className='deleteIcon' />
+                    </div>
+                  </Tooltip>
+                </div>
+              ))
+            }
+          </div>
           <Box padding="15px"></Box>
         </FormControl>
 
@@ -602,8 +741,15 @@ const RegisterLLM = () => {
           </Button>
         </Box>
       </div>
-      <div className={!isShow ? 'jsonBox hide' : 'jsonBox'}>
-        000000
+
+      <div className={isShow ? 'jsonBox' : 'jsonBox hide'}>
+        <div className='jsonBox-header'>JSON Format</div>
+        <Tooltip title="Copy all" placement="top">
+          <FilterNoneIcon className='icon copyIcon' onClick={handleCopy} />
+        </Tooltip>
+        <textarea className='textarea' placeholder="在此输入......" onChange={(e) => handleChangeText(e.target.value)} value="999000">
+          
+        </textarea>
       </div>
     </Box>
   )
@@ -620,6 +766,7 @@ const styles = {
   checkboxWrapper: {
     display: 'flex',
     flexWrap: 'wrap',
-    maxWidth: '80%',
+    alignItems: 'center',
+    width: '100%'
   },
 }
