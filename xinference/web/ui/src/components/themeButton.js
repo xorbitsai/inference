fceb9e0b8fcfbf83df0843deac64ec8b962acda0
Minamiyama <minamiyama@qq.com>
2024-04-20 19:20:33 +0800

    remove debug log

--- xinference/web/ui/src/components/themeButton.js
+++ xinference/web/ui/src/components/themeButton.js
@@ -8,7 +8,6 @@ import { useThemeContext } from './themeContext'
 const ThemeButton = ({ sx }) => {
   const { themeMode, toggleTheme } = useThemeContext()
 
-  console.log('主题：', useThemeContext)
   return (
     <Box sx={sx}>
       <IconButton size="large" onClick={toggleTheme}>
