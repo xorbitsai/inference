da3def9a00d7d60a6bec90a0daf54ef5853f5d4a
Zihann <zihann.xyz@gmail.com>
2024-06-21 04:45:07 +0800

    added tensorizer files deletion

--- xinference/core/worker.py
+++ xinference/core/worker.py
@@ -833,6 +833,14 @@ class WorkerActor(xo.StatelessActor):
             if paths:
                 paths.update([os.path.realpath(path) for path in paths])
 
+        # get tensorizer path
+        from ..model.llm.pytorch.tensorizer_utils import get_tensorizer_dir
+
+        tensorizer_path = get_tensorizer_dir(path)
+        if os.path.isdir(tensorizer_path):
+            files = os.listdir(tensorizer_path)
+            paths.update([os.path.join(tensorizer_path, file) for file in files])
+
         return list(paths)
 
     async def confirm_and_remove_model(self, model_version: str) -> bool:
