4a2596578137cda5020b84a4cbe931d86de2db05
Chengjie Li <109656400+ChengjieLi28@users.noreply.github.com>
2024-07-04 15:36:31 +0800

    ENH: Continuous batching supports `vision` model ability (#1724)

--- xinference/model/llm/pytorch/glm4v.py
+++ xinference/model/llm/pytorch/glm4v.py
@@ -14,6 +14,7 @@
 import base64
 import logging
 import time
+import typing
 import uuid
 from concurrent.futures import ThreadPoolExecutor
 from io import BytesIO
@@ -24,6 +25,7 @@ import requests
 import torch
 from PIL import Image
 
+from ....core.scheduler import InferenceRequest
 from ....types import (
     ChatCompletion,
     ChatCompletionChunk,
@@ -36,6 +38,7 @@ from ....types import (
 from ...utils import select_device
 from ..llm_family import LLMFamilyV1, LLMSpecV1
 from .core import PytorchChatModel, PytorchGenerateConfig
+from .utils import get_max_src_len
 
 logger = logging.getLogger(__name__)
 
@@ -69,7 +72,6 @@ class Glm4VModel(PytorchChatModel):
         if quantization != "none":
             if self._device == "cuda" and self._is_linux():
                 kwargs["device_map"] = "auto"
-                self._device = "auto"
                 if quantization == "4-bit":
                     kwargs["load_in_4bit"] = True
                 elif quantization == "8-bit":
@@ -137,9 +139,6 @@ class Glm4VModel(PytorchChatModel):
                     fut = executor.submit(_load_image, image_url)
                     image_futures.append(fut)
             images = [fut.result() for fut in image_futures]
-            # images = []
-            # for image_url in image_urls:
-            #     images.append(_load_image(image_url))
             text = " ".join(texts)
             if len(images) == 0:
                 return text, []
@@ -149,19 +148,11 @@ class Glm4VModel(PytorchChatModel):
                 raise RuntimeError("Only one image per message is supported")
         return content, []
 
-    def chat(
+    def _get_chat_msgs(
         self,
         prompt: Union[str, List[Dict]],
-        system_prompt: Optional[str] = None,
         chat_history: Optional[List[ChatCompletionMessage]] = None,
-        generate_config: Optional[PytorchGenerateConfig] = None,
-    ) -> Union[ChatCompletion, Iterator[ChatCompletionChunk]]:
-        from transformers import TextIteratorStreamer
-
-        if not generate_config:
-            generate_config = {}
-
-        stream = generate_config.get("stream", False)
+    ):
         content, images_chat = self._message_content_to_chat(prompt)
 
         msgs = []
@@ -170,7 +161,7 @@ class Glm4VModel(PytorchChatModel):
         for h in chat_history or []:
             role = h["role"]
             content_h, images_tmp = self._message_content_to_chat(h["content"])
-            if images_tmp != []:
+            if images_tmp:
                 images_history = images_tmp
             if len(query_to_response) == 0 and role == "user":
                 query_to_response.append({"role": "user", "content": content_h})
@@ -185,6 +176,22 @@ class Glm4VModel(PytorchChatModel):
         elif len(images_history) > 0:
             image = images_history[0]
         msgs.append({"role": "user", "content": content, "image": image})
+        return msgs
+
+    def chat(
+        self,
+        prompt: Union[str, List[Dict]],
+        system_prompt: Optional[str] = None,
+        chat_history: Optional[List[ChatCompletionMessage]] = None,
+        generate_config: Optional[PytorchGenerateConfig] = None,
+    ) -> Union[ChatCompletion, Iterator[ChatCompletionChunk]]:
+        from transformers import TextIteratorStreamer
+
+        if not generate_config:
+            generate_config = {}
+
+        stream = generate_config.get("stream", False)
+        msgs = self._get_chat_msgs(prompt, chat_history)
 
         inputs = self._tokenizer.apply_chat_template(
             msgs,
@@ -282,3 +289,152 @@ class Glm4VModel(PytorchChatModel):
         )
         chunk["usage"] = completion_usage
         yield chunk
+
+    def _get_full_prompt(self, prompt, system_prompt, chat_history, tools):
+        msgs = self._get_chat_msgs(prompt, chat_history)
+        inputs = self._tokenizer.apply_chat_template(
+            msgs,
+            add_generation_prompt=True,
+            tokenize=True,
+            return_tensors="pt",
+            return_dict=True,
+        )
+        return {
+            "input_ids": inputs.input_ids.squeeze(0),
+            "images": inputs.images.squeeze(0),
+        }
+
+    def prepare_sanitize_generate_config(self, req: InferenceRequest):
+        """
+        Refer to https://huggingface.co/THUDM/glm-4v-9b/blob/main/generation_config.json
+        """
+        raw_config = req.inference_kwargs.get("raw_params", {})
+        temperature = raw_config.get("temperature", None)
+        if temperature is None:
+            raw_config["temperature"] = 0.8
+        top_p = raw_config.get("top_p", None)
+        if top_p is None:
+            raw_config["top_p"] = 0.8
+        return raw_config
+
+    def build_prefill_inputs(self, prompts: List, req_list: List[InferenceRequest]):
+        context_len = self.get_context_len()
+        assert isinstance(prompts[0], dict)
+        images = []
+        max_length = float("-inf")
+        for i, feature in enumerate(prompts):
+            req = req_list[i]
+            if "images" in feature:
+                images.append(feature.pop("images", None))
+            max_src_len = get_max_src_len(context_len, req)
+            input_ids = feature["input_ids"][-max_src_len:]
+            req.prompt_tokens = input_ids.tolist()
+            feature["input_ids"] = input_ids
+            max_length = max(len(input_ids), max_length)
+
+        def pad_to_max_length_internal(feature, max_len, idx):
+            padding_length = max_len - len(feature["input_ids"])
+            req_list[idx].padding_len = padding_length
+            feature["input_ids"] = torch.cat(
+                [torch.full((padding_length,), 0), feature["input_ids"]]
+            )
+            return feature
+
+        features = [
+            pad_to_max_length_internal(feature, max_length, i)
+            for i, feature in enumerate(prompts)
+        ]
+        batch = {
+            key: torch.stack([feature[key] for feature in features])
+            for key in features[0].keys()
+        }
+        if images:
+            batch["images"] = torch.stack(images).to(self._device)
+        batch["input_ids"] = batch["input_ids"].to(self._device)
+        return batch
+
+    @staticmethod
+    def is_empty(images_list: Optional[List[List[torch.Tensor]]]):
+        """
+        Copied from https://huggingface.co/THUDM/glm-4v-9b/blob/main/modeling_chatglm.py
+        """
+        if images_list is None or len(images_list) == 0:
+            return True
+        for image_list in images_list:
+            if image_list is not None:
+                return False
+        return True
+
+    @typing.no_type_check
+    def get_full_attention_mask(
+        self, attention_mask, input_ids, images, req_list: List[InferenceRequest]
+    ):
+        """
+        Modified according to https://huggingface.co/THUDM/glm-4v-9b/blob/main/modeling_chatglm.py
+        """
+        image_size: int = self._model.config.vision_config["image_size"]
+        patch_size: int = self._model.config.vision_config["patch_size"]
+        num_patches = (image_size // patch_size // 2) ** 2
+        new_attention_masks = []
+
+        # if not image, use this default id
+        eoi_token_pos = 6
+        boi_token_pos = 4
+
+        for i in range(len(input_ids)):
+            input_id = input_ids[i].tolist()
+            req = req_list[i]
+            if not self.is_empty(images):
+                _boi_token_pos, _eoi_token_pos = input_id.index(
+                    self._model.config.boi_token_id
+                ), input_id.index(self._model.config.eoi_token_id)
+            else:
+                _boi_token_pos = boi_token_pos + req.padding_len
+                _eoi_token_pos = eoi_token_pos + req.padding_len
+            assert eoi_token_pos - boi_token_pos == 2
+            new_attention_masks.append(
+                torch.cat(
+                    (
+                        attention_mask[i, : _boi_token_pos + 1],
+                        attention_mask.new_ones(num_patches),
+                        attention_mask[i, _eoi_token_pos:],
+                    )
+                )
+            )
+        attention_mask = torch.stack(new_attention_masks, dim=0).to(self._device)
+        return attention_mask
+
+    def build_prefill_kwargs(self, prompts: List, req_list: List[InferenceRequest]):
+        batch = self.build_prefill_inputs(prompts, req_list)
+        batch_size, seq_len = batch["input_ids"].shape
+        attention_mask = self.build_prefill_attention_mask(
+            batch_size, seq_len, req_list
+        )
+        if attention_mask is not None:
+            full_attention_mask = self.get_full_attention_mask(
+                attention_mask, batch["input_ids"], batch["images"], req_list
+            )
+            batch["attention_mask"] = full_attention_mask
+            for r in req_list:
+                r.extra_kwargs["attention_mask_seq_len"] = full_attention_mask.shape[1]
+        position_ids = self.build_prefill_position_ids(batch_size, seq_len, req_list)
+        if position_ids is not None:
+            batch["position_ids"] = position_ids
+        return batch
+
+    def build_decode_attention_mask(
+        self, batch_size: int, seq_length: int, reqs: List[InferenceRequest]
+    ):
+        max_seq_len = max(r.extra_kwargs["attention_mask_seq_len"] for r in reqs)
+
+        new_attention_mask = []
+        for r in reqs:
+            attn_mask_seq_len = r.extra_kwargs["attention_mask_seq_len"]
+            pad_len = max_seq_len - attn_mask_seq_len
+            new_attention_mask.append(
+                torch.cat(
+                    [torch.full((pad_len,), 0), torch.ones((attn_mask_seq_len + 1,))]
+                )
+            )
+            r.extra_kwargs["attention_mask_seq_len"] += 1
+        return torch.stack(new_attention_mask, dim=0).to(self._device)
