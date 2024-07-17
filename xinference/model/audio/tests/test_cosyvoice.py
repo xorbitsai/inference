59132b3b48cf0e691dc69337284c042744230541
codingl2k1 <codingl2k1@outlook.com>
2024-07-17 22:45:38 +0200

    Fix CI

--- xinference/model/audio/tests/test_cosyvoice.py
+++ xinference/model/audio/tests/test_cosyvoice.py
@@ -22,7 +22,7 @@ def test_cosyvoice(setup):
     client = Client(endpoint)
 
     model_uid = client.launch_model(
-        model_name="CosyVoice-SFT",
+        model_name="CosyVoice-300M-SFT",
         model_type="audio",
     )
     model = client.get_model(model_uid)
