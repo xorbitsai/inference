d33e063e3e47b55b01a43f4dfbadd28ec730f881
qinxuye <qinxuye@gmail.com>
2024-08-14 11:34:48 +0800

    Revert

--- xinference/model/llm/vllm/core.py
+++ xinference/model/llm/vllm/core.py
@@ -148,6 +148,8 @@ if VLLM_INSTALLED and vllm.__version__ >= "0.3.3":
     VLLM_SUPPORTED_CHAT_MODELS.append("orion-chat-rag")
 
 if VLLM_INSTALLED and vllm.__version__ >= "0.4.0":
+    VLLM_SUPPORTED_CHAT_MODELS.append("qwen1.5-moe-chat")
+    VLLM_SUPPORTED_CHAT_MODELS.append("qwen2-moe-instruct")
     VLLM_SUPPORTED_CHAT_MODELS.append("c4ai-command-r-v01")
 
 if VLLM_INSTALLED and vllm.__version__ >= "0.5.3":
