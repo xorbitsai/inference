de805321aca574ec44b93fb52fefcd178c16f41c
Zzzz1111 <34296741+Zzzz1111@users.noreply.github.com>
2024-08-29 15:00:01 +0800

    BUG: 🐛 fix unable launch qwen2-embedding (#2185)
    Co-authored-by: 胡子俊 <huzijun@cvte.com>

--- xinference/model/embedding/core.py
+++ xinference/model/embedding/core.py
@@ -124,6 +124,7 @@ class EmbeddingModel:
         model_path: str,
         model_spec: EmbeddingModelSpec,
         device: Optional[str] = None,
+        **kwargs,
     ):
         self._model_uid = model_uid
         self._model_path = model_path
@@ -131,6 +132,7 @@ class EmbeddingModel:
         self._model = None
         self._counter = 0
         self._model_spec = model_spec
+        self._kwargs = kwargs
 
     def load(self):
         try:
