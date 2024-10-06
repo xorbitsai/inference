2ffe0459934899e4aef2d0e27459ff913a1f1087
qinxuye <qinxuye@gmail.com>
2024-10-06 16:34:22 +0800

    fix

--- xinference/model/image/stable_diffusion/core.py
+++ xinference/model/image/stable_diffusion/core.py
@@ -404,7 +404,7 @@ class DiffusionModel(SDAPIDiffusionModelMixin):
     def _process_progressor(kwargs: dict):
         import diffusers
 
-        progressor: Progressor = kwargs.pop("progressor")
+        progressor: Progressor = kwargs.pop("progressor", None)
 
         def report_status_callback(
             pipe: diffusers.DiffusionPipeline,
@@ -417,7 +417,7 @@ class DiffusionModel(SDAPIDiffusionModelMixin):
 
             return callback_kwargs
 
-        if progressor.request_id:
+        if progressor and progressor.request_id:
             kwargs["callback_on_step_end"] = report_status_callback
 
     def _call_model(
