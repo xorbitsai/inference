0cdfb43fb27b1bf3667525f45d3fff00f1b15874
codingl2k1 <138426806+codingl2k1@users.noreply.github.com>
2024-11-19 14:24:22 +0100

    ENH: Support fish speech reference audio (#2542)

--- xinference/model/audio/tests/test_fish_speech.py
+++ xinference/model/audio/tests/test_fish_speech.py
@@ -22,8 +22,7 @@ def test_fish_speech(setup):
     client = Client(endpoint)
 
     model_uid = client.launch_model(
-        model_name="FishSpeech-1.4",
-        model_type="audio",
+        model_name="FishSpeech-1.4", model_type="audio", compile=False
     )
     model = client.get_model(model_uid)
     input_string = "你好，你是谁？"
