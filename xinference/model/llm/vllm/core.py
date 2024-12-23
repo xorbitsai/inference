a41e5f42da91f012a7ac42dd8d5a9da518a9599e
JunHowie <JunHowie@aliyun.com>
2024-12-23 17:43:17 +0800

    feat：Support minicpm-4B on vllm

--- xinference/model/llm/vllm/core.py
+++ xinference/model/llm/vllm/core.py
@@ -187,11 +187,13 @@ if VLLM_INSTALLED and vllm.__version__ > "0.5.3":
 if VLLM_INSTALLED and vllm.__version__ >= "0.6.1":
     VLLM_SUPPORTED_VISION_MODEL_LIST.append("internvl2")
 
+if VLLM_INSTALLED and vllm.__version__ >= "0.6.2":
+    VLLM_SUPPORTED_CHAT_MODELS.append("minicpm3-4b")
+
 if VLLM_INSTALLED and vllm.__version__ >= "0.6.3":
     VLLM_SUPPORTED_MODELS.append("llama-3.2-vision")
     VLLM_SUPPORTED_VISION_MODEL_LIST.append("llama-3.2-vision-instruct")
     VLLM_SUPPORTED_VISION_MODEL_LIST.append("qwen2-vl-instruct")
-
 
 class VLLMModel(LLM):
     def __init__(
