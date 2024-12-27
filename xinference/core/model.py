456940a213f38d0e6c058edc9aff068e17781044
JunHowie <JunHowie@aliyun.com>
2024-12-27 16:31:29 +0800

    feat：Support QvQ-72B-Preview

--- xinference/core/model.py
+++ xinference/core/model.py
@@ -75,8 +75,6 @@ XINFERENCE_BATCHING_ALLOWED_VISION_MODELS = [
     "cogvlm2",
     "glm-4v",
     "MiniCPM-V-2.6",
-    "qwen2-vl-instruct",
-    "QvQ-72B-Preview",
 ]
 
 XINFERENCE_TEXT_TO_IMAGE_BATCHING_ALLOWED_MODELS = ["FLUX.1-dev", "FLUX.1-schnell"]
