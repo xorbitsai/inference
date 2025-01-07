74892a65f21c04ad57a250032010d09a1aeaf97f
Minamiyama <minamiyama@qq.com>
2025-01-07 12:35:33 +0800

    FEAT: [UI] theme switch (#1335)

---
+++ xinference/web/ui/src/components/themeButton.js
@@ -0,0 +1,20 @@
+import DarkModeIcon from '@mui/icons-material/DarkMode'
+import LightModeIcon from '@mui/icons-material/LightMode'
+import { Box, IconButton } from '@mui/material'
+import React from 'react'
+
+import { useThemeContext } from './themeContext'
+
+const ThemeButton = ({ sx }) => {
+  const { themeMode, toggleTheme } = useThemeContext()
+
+  return (
+    <Box sx={sx}>
+      <IconButton size="large" onClick={toggleTheme}>
+        {themeMode === 'light' ? <LightModeIcon /> : <DarkModeIcon />}
+      </IconButton>
+    </Box>
+  )
+}
+
+export default ThemeButton
