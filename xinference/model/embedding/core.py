39747c0e6de88a6242d15eb42614bb7e223078b6
zhoudelong <1727964916@qq.com>
2025-01-23 10:34:45 +0800

    BLD: fix create_embedding field model_replica

--- xinference/model/embedding/core.py
+++ xinference/model/embedding/core.py
@@ -693,7 +693,7 @@ class EmbeddingModel:
                 if not is_bge_m3_flag_model and not kwargs.get("return_sparse")
                 else "dict"
             ),
-            model="",
+            # model="",
             model_replica=self._model_uid,
             data=embedding_list,
             usage=usage,
