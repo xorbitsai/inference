7e456e1bf3f5505e222b18ef663ed2d75c2743d1
zhoudelong <1727964916@qq.com>
2025-02-14 11:19:30 +0800

    out model=''

--- xinference/model/embedding/core.py
+++ xinference/model/embedding/core.py
@@ -693,7 +693,6 @@ class EmbeddingModel:
                 if not is_bge_m3_flag_model and not kwargs.get("return_sparse")
                 else "dict"
             ),
-            # model="",
             model_replica=self._model_uid,
             data=embedding_list,
             usage=usage,
