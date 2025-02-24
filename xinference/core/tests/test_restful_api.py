e5920e782fc3a2a0fd76096e0f2f53ea3dbac993
zhoudelong <1727964916@qq.com>
2025-02-24 18:02:21 +0800

    add: test embedding field model_replica

--- xinference/core/tests/test_restful_api.py
+++ xinference/core/tests/test_restful_api.py
@@ -356,6 +356,7 @@ def test_restful_api_for_embedding(setup):
 
     assert "embedding" in embedding_res["data"][0]
     assert len(embedding_res["data"][0]["embedding"]) == model_spec.dimensions
+    assert "model_replica" in embedding_res["data"][0]
 
     # test multiple
     payload = {
