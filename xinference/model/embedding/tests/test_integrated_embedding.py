850f7aed340a3a9a4160d776f779d41f6be36891
Example User <example@example.com>
2025-03-04 10:12:50 +0000

    Bug

--- xinference/model/embedding/tests/test_integrated_embedding.py
+++ xinference/model/embedding/tests/test_integrated_embedding.py
@@ -26,7 +26,7 @@ def test_sparse_embedding(setup):
     client = Client(endpoint)
 
     model_uid = client.launch_model(
-        model_name="bge-m3", model_type="embedding", hybrid_mode=True,model_hub="modelscope",model_path="/home/zhongmingwei/.xinference/cache/bge-m3"
+        model_name="bge-m3", model_type="embedding", hybrid_mode=True
     )
     assert len(client.list_models()) == 1
 
