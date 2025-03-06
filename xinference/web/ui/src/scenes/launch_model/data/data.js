3ed055253df3ac6d3a8cfbd9f4ce0ec88e3564dc
yiboyasss <3359595624@qq.com>
2025-03-06 17:08:06 +0800

    refactor: move featureModels to data.js

--- xinference/web/ui/src/scenes/launch_model/data/data.js
+++ xinference/web/ui/src/scenes/launch_model/data/data.js
@@ -52,3 +52,58 @@ export const additionalParameterTipList = {
   ],
   'mlx': ['cache_limit_gb', 'max_kv_size'],
 }
+
+export const featureModels = [
+  {
+    type: 'llm',
+    feature_models: [
+      'deepseek-v3',
+      'deepseek-r1',
+      'deepseek-r1-distill-qwen',
+      'deepseek-r1-distill-llama',
+      'qwen2.5-instruct',
+      'qwen2.5-vl-instruct',
+      'qwen2.5-coder-instruct',
+      'llama-3.1-instruct',
+    ],
+  },
+  {
+    type: 'embedding',
+    feature_models: [
+      'bge-large-zh-v1.5',
+      'bge-large-en-v1.5',
+      'bge-m3',
+      'gte-Qwen2',
+      'jina-embeddings-v3',
+    ],
+  },
+  {
+    type: 'rerank',
+    feature_models: [],
+  },
+  {
+    type: 'image',
+    feature_models: [
+      'FLUX.1-dev',
+      'FLUX.1-schnell',
+      'sd3.5-large',
+      'HunyuanDiT-v1.2',
+      'sd3.5-medium',
+    ],
+  },
+  {
+    type: 'audio',
+    feature_models: [
+      'CosyVoice2-0.5B',
+      'FishSpeech-1.5',
+      'F5-TTS',
+      'ChatTTS',
+      'SenseVoiceSmall',
+      'whisper-large-v3',
+    ],
+  },
+  {
+    type: 'video',
+    feature_models: [],
+  },
+]
