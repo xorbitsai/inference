243669ed7c8bb43d261f990fe39f6a38c0d08d0f
kaiyuan Zhang <zky21@mails.tsinghua.edu.cn>
2025-03-17 02:59:54 +0000

    FEAT: Support gemma-3

--- xinference/model/llm/sglang/core.py
+++ xinference/model/llm/sglang/core.py
@@ -94,6 +94,7 @@ SGLANG_SUPPORTED_CHAT_MODELS = [
     "mixtral-instruct-v0.1",
     "gemma-it",
     "gemma-2-it",
+    "gemma-3-it",
     "deepseek-v2.5",
     "deepseek-v2-chat",
     "deepseek-v2-chat-0628",
