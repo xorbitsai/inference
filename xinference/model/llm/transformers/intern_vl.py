49e7e969d98dec57c518e38c953715d1e2a97a7b
Minamiyama <minamiyama@qq.com>
2025-04-12 22:23:00 +0800

    reformatted

--- xinference/model/llm/transformers/intern_vl.py
+++ xinference/model/llm/transformers/intern_vl.py
@@ -272,9 +272,7 @@ class InternVLChatModel(PytorchChatModel):
         model_name = self.model_family.model_name.lower().replace("-mpo", "")
         model_name = f"{model_name}-{model_size}"
         if "internvl3" in model_name.lower():
-            config = AutoConfig.from_pretrained(
-                self.model_path, trust_remote_code=True
-            )
+            config = AutoConfig.from_pretrained(self.model_path, trust_remote_code=True)
             num_layers = config.llm_config.num_hidden_layers
         else:
             num_layers = {
