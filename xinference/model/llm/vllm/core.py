46af2d7acdad67ae9956f55eab9cecb2a9bc5760
Jun-Howie <JunHowie@aliyun.com>
2025-04-17 11:15:59 +0800

    FEAT:support skywork-or1-preview

--- xinference/model/llm/vllm/core.py
+++ xinference/model/llm/vllm/core.py
@@ -175,6 +175,7 @@ if VLLM_INSTALLED and vllm.__version__ >= "0.3.0":
     VLLM_SUPPORTED_CHAT_MODELS.append("deepseek-r1-distill-qwen")
     VLLM_SUPPORTED_CHAT_MODELS.append("fin-r1")
     VLLM_SUPPORTED_CHAT_MODELS.append("seallms-v3")
+    VLLM_SUPPORTED_CHAT_MODELS.append("skywork-or1-preview")
 
 if VLLM_INSTALLED and vllm.__version__ >= "0.3.2":
     VLLM_SUPPORTED_CHAT_MODELS.append("gemma-it")
