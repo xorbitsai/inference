993c7393a2622e515428b57d7acbe228be109e58
Jun-Howie <JunHowie@aliyun.com>
2025-04-28 21:45:27 +0800

    FEAT: Support Qwen3 and Qwen3MOE

--- xinference/model/llm/vllm/core.py
+++ xinference/model/llm/vllm/core.py
@@ -243,6 +243,10 @@ if VLLM_INSTALLED and vllm.__version__ >= "0.8.0":
 
 if VLLM_INSTALLED and vllm.__version__ >= "0.8.4":
     VLLM_SUPPORTED_CHAT_MODELS.append("glm4-0414")
+    VLLM_SUPPORTED_CHAT_MODELS.append("qwen3")
+
+if VLLM_INSTALLED and vllm.__version__ >= "0.8.5":
+    VLLM_SUPPORTED_CHAT_MODELS.append("qwen3")
 
 if VLLM_INSTALLED and vllm.__version__ >= "0.8.5":
     VLLM_SUPPORTED_CHAT_MODELS.append("qwen3")
