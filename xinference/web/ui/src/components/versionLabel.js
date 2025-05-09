d9b7b724a2238888cb53556c24133c3c6e8a238e
Minamiyama <minamiyama@qq.com>
2025-05-09 22:17:00 +0800

    remove comments

--- xinference/web/ui/src/components/versionLabel.js
+++ xinference/web/ui/src/components/versionLabel.js
@@ -4,10 +4,8 @@ import React, { useEffect, useState } from 'react'
 import fetchWrapper from '../components/fetchWrapper'
 
 const VersionLabel = ({ sx }) => {
-  // 版本号状态变量，方便后续通过网络请求更新
   const [version, setVersion] = useState('')
 
-  // 组件加载后获取版本信息
   useEffect(() => {
     fetchWrapper
       .get('/v1/cluster/version')
