2f20367e58eb7eebc3a0389fcdcc46ef1ec7a33e
Minamiyama <minamiyama@qq.com>
2025-05-11 18:32:55 +0800

    WIP

--- xinference/model/embedding/__init__.py
+++ xinference/model/embedding/__init__.py
@@ -54,17 +54,6 @@ def register_custom_model():
             except Exception as e:
                 warnings.warn(f"{user_defined_embedding_dir}/{f} has error, {e}")
 
-def load_model_family_from_json(json_filename, target_families):
-    json_path = os.path.join(os.path.dirname(__file__), json_filename)
-    target_families.update(
-        dict(
-            (spec["model_name"], EmbeddingModelSpec(**spec))
-            for spec in json.load(codecs.open(json_path, "r", encoding="utf-8"))
-        )
-    )
-    for model_name, model_spec in target_families.items():
-        MODEL_NAME_TO_REVISION[model_name].append(model_spec.model_revision)
-
 def _install():
     load_model_family_from_json("model_spec.json", BUILTIN_EMBEDDING_MODELS)
     load_model_family_from_json("model_spec_modelscope.json", MODELSCOPE_EMBEDDING_MODELS)
@@ -84,3 +73,14 @@ def _install():
         EMBEDDING_MODEL_DESCRIPTIONS.update(
             generate_embedding_description(ud_embedding)
         )
+
+def load_model_family_from_json(json_filename, target_families):
+    json_path = os.path.join(os.path.dirname(__file__), json_filename)
+    target_families.update(
+        dict(
+            (spec["model_name"], EmbeddingModelSpec(**spec))
+            for spec in json.load(codecs.open(json_path, "r", encoding="utf-8"))
+        )
+    )
+    for model_name, model_spec in target_families.items():
+        MODEL_NAME_TO_REVISION[model_name].append(model_spec.model_revision)
