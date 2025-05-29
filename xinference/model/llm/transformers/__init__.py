220984a7dc61318a2eac44466f27e8d70b246c71
Gmgge <157505853@qq.com>
2025-05-29 20:33:00 +0800

    chore: Apply code style fixes using black

--- xinference/model/llm/transformers/__init__.py
+++ xinference/model/llm/transformers/__init__.py
@@ -26,7 +26,9 @@ def import_submodules(package_path: str, package_name: str, globals_dict: Dict) 
     for _, module_name, is_pkg in pkgutil.iter_modules([package_path]):
         full_module_name = f"{package_name}.{module_name}"
 
-        if module_name.startswith(("_", "test_")):  # Skip the modules which start with "_" or "test_"
+        if module_name.startswith(
+            ("_", "test_")
+        ):  # Skip the modules which start with "_" or "test_"
             continue
 
         module = importlib.import_module(full_module_name)
