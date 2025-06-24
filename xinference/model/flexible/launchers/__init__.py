0b7622be41c1e3004817aef97c818b940feb65b3
qinxuye <qinxuye@gmail.com>
2025-06-24 23:41:14 +0800

    FEAT: support yolo launcher

--- xinference/model/flexible/launchers/__init__.py
+++ xinference/model/flexible/launchers/__init__.py
@@ -15,3 +15,4 @@
 from .image_process_launcher import launcher as image_process
 from .modelscope_launcher import launcher as modelscope
 from .transformers_launcher import launcher as transformers
+from .yolo_launcher import launcher as yolo
