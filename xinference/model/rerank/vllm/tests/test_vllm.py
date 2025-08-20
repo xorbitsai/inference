2e0548c94be565674bfe3c3025e7644942fa5c05
bash000000 <m2588953@outlook.com>
2025-08-20 16:15:08 +0800

    Update

--- xinference/model/rerank/vllm/tests/test_vllm.py
+++ xinference/model/rerank/vllm/tests/test_vllm.py
@@ -2,6 +2,7 @@ import shutil
 
 import pytest
 
+from .....client import Client
 from ...cache_manager import RerankCacheManager
 from ...core import RerankModelFamilyV2, RerankSpecV1
 from ..core import VLLMRerankModel
@@ -27,7 +28,7 @@ def test_model():
     model_path = None
     try:
         model_path = RerankCacheManager(TEST_MODEL_SPEC).cache()
-        model = VLLMRerankModel("mock", model_path, TEST_MODEL_SPEC)
+        model = VLLMRerankModel("mock", model_path, TEST_MODEL_SPEC, "none")
 
         query = "A man is eating pasta."
         # With all sentences in the corpus
@@ -50,3 +51,34 @@ def test_model():
     finally:
         if model_path is not None:
             shutil.rmtree(model_path, ignore_errors=True)
+
+
+@pytest.mark.skipif(not VLLMRerankModel.check_lib(), reason="vllm not installed")
+def test_qwen3_vllm(setup):
+    endpoint, _ = setup
+    client = Client(endpoint)
+    model_uid = client.launch_model(
+        model_name="Qwen3-Reranker-0.6B",
+        model_type="rerank",
+        model_engine="vllm",
+    )
+
+    model = client.get_model(model_uid)
+    # We want to compute the similarity between the query sentence
+    query = "A man is eating pasta."
+
+    # With all sentences in the corpus
+    corpus = [
+        "A man is eating food.",
+        "A man is eating a piece of bread.",
+        "The girl is carrying a baby.",
+        "A man is riding a horse.",
+        "A woman is playing violin.",
+        "Two men pushed carts through the woods.",
+        "A man is riding a white horse on an enclosed ground.",
+        "A monkey is playing drums.",
+        "A cheetah is running behind its prey.",
+    ]
+
+    scores = model.rerank(corpus, query, return_documents=True)
+    assert scores["results"][0]["index"] == 1
