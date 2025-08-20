2367876deab45496444e29258a7d3e49d44b13ad
Xuye Qin <qinxuye@gmail.com>
2025-08-20 11:10:04 +0800

    BUG: fix rerank model creation (#3977)

--- xinference/model/rerank/sentence_transformers/core.py
+++ xinference/model/rerank/sentence_transformers/core.py
@@ -72,7 +72,7 @@ class SentenceTransformerRerankModel(RerankModel):
         enable_flash_attn = self._kwargs.pop(
             "enable_flash_attn", is_flash_attn_available()
         )
-        if self._auto_detect_type(self._model_path) != "normal" and enable_flash_attn:
+        if enable_flash_attn:
             logger.warning(
                 "flash_attn can only support fp16 and bf16, will force set `use_fp16` to True"
             )
