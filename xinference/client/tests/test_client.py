a2da7cab9fd5f301ff32d768ff78e77ca79e42c1
codingl2k1 <138426806+codingl2k1@users.noreply.github.com>
2025-08-28 18:55:54 +0800

    Update test_client.py
    Fix UT

--- xinference/client/tests/test_client.py
+++ xinference/client/tests/test_client.py
@@ -83,6 +83,9 @@ def test_RESTful_client(setup):
             generate_config={"stream": True, "max_tokens": 5},
         )
         for chunk in streaming_response:
+            if not chunk["choices"]:
+                assert chunk["usage"]
+                continue
             assert "finish_reason" in chunk["choices"][0]
             finish_reason = chunk["choices"][0]["finish_reason"]
             if finish_reason is None:
