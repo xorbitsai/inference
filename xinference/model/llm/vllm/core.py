a7148c8cf8916006f082dd6cf487d0efbec32564
JunHowie <JunHowie@aliyun.com>
2025-09-29 17:28:25 +0800

    FEAT:Support Qwen3-VL models，quantization model and tool_call

--- xinference/model/llm/vllm/core.py
+++ xinference/model/llm/vllm/core.py
@@ -295,6 +295,10 @@ if VLLM_INSTALLED and VLLM_VERSION > version.parse("0.10.2"):
     VLLM_SUPPORTED_VISION_MODEL_LIST.append("Qwen3-VL-Instruct")
     VLLM_SUPPORTED_VISION_MODEL_LIST.append("Qwen3-VL-Instruct")
 
+if VLLM_INSTALLED and VLLM_VERSION >= version.parse("0.11.0"):
+    VLLM_SUPPORTED_VISION_MODEL_LIST.append("Qwen3-VL-Instruct")
+    VLLM_SUPPORTED_VISION_MODEL_LIST.append("Qwen3-VL-Instruct")
+
 
 class VLLMModel(LLM):
     def __init__(
