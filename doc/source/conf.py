d163d1fc4df8b50c419a4055c81a96e4dc68886d
qinxuye <qinxuye@gmail.com>
2026-05-10 20:37:25 +0800

    Polish docs header layout

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -66,6 +66,7 @@ html_title = "Xinference"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_css_files = ["custom.css"]
 
 # Define the json_url for our version switcher.
 version_match = os.environ.get("READTHEDOCS_LANGUAGE")
