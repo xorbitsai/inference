3874e3d8c2720ec487391302804d45e49b773f54
OliverBryant <2713999266@qq.com>
2026-07-03 16:53:21 +0800

    test: keep continuous batching prompts unchanged

--- xinference/core/tests/test_continuous_batching.py
+++ xinference/core/tests/test_continuous_batching.py
@@ -175,10 +175,7 @@ def test_continuous_batching(setup):
 
     # test same request ids
     thread1 = InferenceThread(
-        "请用中文详细介绍人工智能的发展历史，至少列出十个阶段。",
-        {"stream": True, "request_id": "aaabbb", "max_tokens": 512},
-        client,
-        model,
+        "1+1=3正确吗？", {"stream": True, "request_id": "aaabbb"}, client, model
     )
     thread2 = InferenceThreadWithError(
         "中国的首都是哪座城市？",
@@ -194,10 +191,7 @@ def test_continuous_batching(setup):
 
     # test abort request for stream
     thread1 = InferenceThread(
-        "请用中文详细介绍猫和狗的区别，至少从十个方面展开。",
-        {"stream": True, "request_id": "aaabbb", "max_tokens": 512},
-        client,
-        model,
+        "猫和狗有什么区别吗？", {"stream": True, "request_id": "aaabbb"}, client, model
     )
     thread2 = AbortThread(
         client, model_uid_res, "bbbaaa", AbortRequestMessage.NOT_FOUND.name
@@ -218,10 +212,7 @@ def test_continuous_batching(setup):
 
     # test abort request for non-stream
     thread1 = InferenceThread(
-        "请用中文详细介绍猫和狗的区别，至少从十个方面展开。",
-        {"request_id": "aaabbb", "max_tokens": 512},
-        client,
-        model,
+        "猫和狗有什么区别吗？", {"request_id": "aaabbb"}, client, model
     )
     thread2 = AbortThread(
         client, model_uid_res, "aaabbb", AbortRequestMessage.DONE.name, 0.1
